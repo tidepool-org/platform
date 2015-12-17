6708170b895d375002de5627c2772ce713af6e7c
Jamie <jamie@tidepool.org>
2015-12-18 10:15:03 +1300

    add empty store for next stage

---
+++ store/store.go
@@ -0,0 +1,21 @@
+package store
+
+type Store interface {
+	Save(d interface{}) error
+	Update(d interface{}) error
+	Delete(id string) error
+	Read(id string) (interface{}, error)
+	ReadAll() ([]interface{}, error)
+}
+
+type MongoStore struct{}
+
+func (this *MongoStore) Save(d interface{}) error { return nil }
+
+func (this *MongoStore) Update(d interface{}) error { return nil }
+
+func (this *MongoStore) Delete(id string) error { return nil }
+
+func (this *MongoStore) Read(id string) (interface{}, error) { return nil, nil }
+
+func (this *MongoStore) ReadAll() ([]interface{}, error) { return nil, nil }
---
+++ store/store_test.go
@@ -0,0 +1,12 @@
+package store_test
+
+import (
+	. "github.com/tidepool-org/platform/store"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Store", func() {
+
+})
