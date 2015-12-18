df97eae104b30a78aa0e2865f50724775f5928fc
Jamie <jamie@tidepool.org>
2015-12-18 13:14:39 +1300

    fake store test

--- store/store_test.go
+++ store/store_test.go
@@ -9,4 +9,11 @@ import (
 
 var _ = Describe("Store", func() {
 
+	Context("Can be created", func() {
+		It("and should match the interface", func() {
+			var testStore Store
+			testStore = NewMongoStore()
+			Expect(testStore).To(Not(BeNil()))
+		})
+	})
 })
