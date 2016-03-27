fbb8a39a5057367afac04aeeb68295a127625b40
Jamie <jamie@tidepool.org>
2016-03-27 13:21:26 +1300

    cleanup validate to just what we need

--- validate/validate.go
+++ validate/validate.go
@@ -1,19 +1,15 @@
 package validate
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"reflect"
-	"strings"
 
 	"github.com/tidepool-org/platform/Godeps/_workspace/src/gopkg.in/bluesuncorp/validator.v8"
 )
 
 //Validator interface
 type Validator interface {
-	Struct(s interface{}, errs *ErrorsArray)
-	Field(field interface{}, tag ValidationTag) Errors
+	Struct(s interface{}, errorProcessing ErrorProcessing)
+	//Field(field interface{}, tag ValidationTag) Errors
 	RegisterValidation(tag ValidationTag, fn validator.Func)
 }
 
@@ -23,77 +19,12 @@ type PlatformValidator struct {
 	reasons  ErrorReasons
 }
 
-// FieldError contains a single field's validation error along
-// with other properties that may be needed for error message creation
-type FieldError struct {
-	Field string
-	Tag   string
-	Type  reflect.Type
-	Value interface{}
-}
-
 //ValidationTag that all tags will be of this type
 type ValidationTag string
-
-// Errors is a type of map[string]*FieldError
-// it exists to allow for multiple errors to be passed from this library
-// and yet still subscribe to the error interface
-type Errors map[string]*FieldError
 
 // ErrorReasons is a type of map[ValidationTag]string
 // it allows us to map a ValidationTag to a reason why the validation failed
 type ErrorReasons map[ValidationTag]string
-
-// Error is intended for use in development + debugging and not intended to be a production error message.
-// It allows ValidationErrors to subscribe to the Error interface.
-// All information to create an error message specific to your application is contained within
-// the FieldError found within the ValidationErrors map
-func (e Errors) Error() string {
-	const fieldErrMsg = "Key: '%s' Error:Field validation for '%s' failed on the '%s' tag"
-	if len(e) > 0 {
-
-		buff := bytes.NewBufferString("")
-
-		for key, err := range e {
-			buff.WriteString(fmt.Sprintf(fieldErrMsg, key, err.Field, err.Tag))
-			buff.WriteString("\n")
-		}
-
-		return strings.TrimSpace(buff.String())
-	}
-	return ""
-}
-
-//GetError returns a formatted error message for the user
-func (e Errors) GetError(reasons ErrorReasons) error {
-
-	const (
-		fieldErrorMsg = "Field validation for '%s' failed with '%s' when given '%v' for type '%s' "
-		tagErrorMsg   = "Field validation failed with '%s' when given '%v' for type '%s'"
-	)
-
-	if len(e) > 0 {
-		buff := bytes.NewBufferString("")
-
-		for _, err := range e {
-
-			if reason, ok := reasons[ValidationTag(err.Tag)]; ok {
-
-				if err.Field == "" {
-					buff.WriteString(fmt.Sprintf(tagErrorMsg, reason, err.Value, err.Type))
-				} else {
-					buff.WriteString(fmt.Sprintf(fieldErrorMsg, err.Field, reason, err.Value, err.Type))
-				}
-
-			} else {
-				buff.WriteString(fmt.Sprintf(fieldErrorMsg, err.Field, err.Tag, err.Value, err.Type))
-			}
-			buff.WriteString("\n")
-		}
-		return errors.New(strings.TrimSpace(buff.String()))
-	}
-	return nil
-}
 
 //NewPlatformValidator returns initialised PlatformValidator with custom tidepool validation
 func NewPlatformValidator() *PlatformValidator {
@@ -104,14 +35,6 @@ func NewPlatformValidator() *PlatformValidator {
 func (pv *PlatformValidator) SetErrorReasons(reasons ErrorReasons) *PlatformValidator {
 	pv.reasons = reasons
 	return pv
-}
-
-func toErrors(ve validator.ValidationErrors) Errors {
-	errs := Errors{}
-	for k, v := range ve {
-		errs[k] = &FieldError{Field: v.Field, Tag: v.Tag, Type: v.Type, Value: v.Value}
-	}
-	return errs
 }
 
 func (pv *PlatformValidator) toErrorsArray(ve validator.ValidationErrors, errorProcessing ErrorProcessing) {
@@ -129,14 +52,6 @@ func (pv *PlatformValidator) toErrorsArray(ve validator.ValidationErrors, errorP
 //Struct validation for the PlatformValidator
 func (pv *PlatformValidator) Struct(s interface{}, errorProcessing ErrorProcessing) {
 	validationErrors := pv.validate.Struct(s)
-	if validationErrors != nil {
-		pv.toErrorsArray(validationErrors.(validator.ValidationErrors), errorProcessing)
-	}
-}
-
-//Field for the PlatformValidator
-func (pv *PlatformValidator) Field(field interface{}, tag ValidationTag, errorProcessing ErrorProcessing) {
-	validationErrors := pv.validate.Field(field, string(tag))
 	if validationErrors != nil {
 		pv.toErrorsArray(validationErrors.(validator.ValidationErrors), errorProcessing)
 	}
--- validate/validate_test.go
+++ validate/validate_test.go
@@ -12,82 +12,10 @@ import (
 
 var _ = Describe("Validate", func() {
 
-	Context("basic struct", func() {
-		type testStruct struct {
-			ID      string      `json:"id"  valid:"required"`
-			Type    string      `json:"type"  valid:"required"`
-			Offset  int         `json:"offset,omitempty"  valid:"omitempty,min=-10,max=10"`
-			Payload interface{} `json:"payload,omitempty"  valid:"-"`
+	Context("using custom validator", func() {
+		type ValidationTest struct {
+			Offset int `json:"offset" valid:"custom"`
 		}
-		var (
-			validator = NewPlatformValidator()
-		)
-		Context("is valid", func() {
-			It("all feilds set correctly", func() {
-				all := testStruct{ID: "someid", Type: "test", Offset: -9, Payload: map[string]string{"some": "stuff"}}
-				Expect(validator.Struct(all)).To(BeNil())
-			})
-			It("only core feilds set correctly", func() {
-				core := testStruct{ID: "someid", Type: "test"}
-				Expect(validator.Struct(core)).To(BeNil())
-			})
-		})
-		Context("is invalid when", func() {
-			It("expected feilds aren't set", func() {
-				none := testStruct{}
-				Expect(validator.Struct(none)).ToNot(BeNil())
-			})
-			It("expected feilds are incorrectly set", func() {
-				invalid := testStruct{ID: "", Type: ""}
-				Expect(validator.Struct(invalid)).ToNot(BeNil())
-			})
-			It("optional feild set are incorrectly", func() {
-				invalid := testStruct{ID: "someid", Type: "a_type", Offset: -100}
-				Expect(validator.Struct(invalid)).ToNot(BeNil())
-			})
-		})
-	})
-
-	Context("nested struct", func() {
-		type nestedUser struct {
-			Firstname string `json:"firstname"  valid:"required"`
-			Lastname  string `json:"lastname,omitempty"  valid:"omitempty,required"`
-		}
-		type testStruct struct {
-			ID     string `json:"id"  valid:"required"`
-			Offset int    `json:"offset,omitempty"  valid:"omitempty,required"`
-			User   nestedUser
-		}
-		var (
-			validator = NewPlatformValidator()
-		)
-		Context("is valid when", func() {
-			It("all feilds set correctly", func() {
-				all := testStruct{ID: "someid", Offset: 9, User: nestedUser{Firstname: "first", Lastname: "last"}}
-				Expect(validator.Struct(all)).To(BeNil())
-			})
-			It("only core feilds set correctly", func() {
-				core := testStruct{ID: "someid", User: nestedUser{Firstname: "first"}}
-				Expect(validator.Struct(core)).To(BeNil())
-			})
-		})
-		Context("is invalid when", func() {
-			It("expected feilds aren't set", func() {
-				none := testStruct{}
-				Expect(validator.Struct(none)).ToNot(BeNil())
-			})
-			It("nested core feilds aren't set", func() {
-				core := testStruct{ID: "someid", User: nestedUser{Firstname: ""}}
-				Expect(validator.Struct(core)).ToNot(BeNil())
-			})
-			It("expected feilds are incorrectly set", func() {
-				invalid := testStruct{ID: "", User: nestedUser{Firstname: ""}}
-				Expect(validator.Struct(invalid)).ToNot(BeNil())
-			})
-		})
-	})
-
-	Context("custom validator", func() {
 		var (
 			testValidator = func(v *valid.Validate, topStruct reflect.Value, currentStructOrField reflect.Value, field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string) bool {
 				//a place holder for more through validation
@@ -102,42 +30,40 @@ var _ = Describe("Validate", func() {
 				"custom": "Bad offset sorry",
 			}
 			validator = NewPlatformValidator()
+
+			processing = ErrorProcessing{BasePath: "0/validationtest", ErrorsArray: NewErrorsArray()}
 		)
-		type ValidationTest struct {
-			Offset int `json:"offset" valid:"custom"`
-		}
 
 		BeforeEach(func() {
 			validator.RegisterValidation("custom", testValidator)
 			validator.SetErrorReasons(validationFailureReasons)
 		})
 
-		It("fails struct validation ", func() {
-			none := ValidationTest{Offset: 0}
-			Expect(validator.Struct(none)).ToNot(BeNil())
+		Context("succeeds", func() {
+
+			It("when offset match's expected value", func() {
+				none := ValidationTest{Offset: 5}
+				validator.Struct(none, processing)
+				Expect(processing.ErrorsArray.HasErrors()).To(BeFalse())
+			})
 		})
-		It("fails struct validation gives meaningfull message", func() {
-			none := ValidationTest{Offset: 0}
-			errs := validator.Struct(none)
-			if errs != nil {
-				Expect(errs.Errors[0].Detail).To(ContainSubstring("Field validation for 'Offset' failed with 'Bad offset sorry' when given '0' for type 'int'"))
-			}
+
+		Context("fails", func() {
+
+			It("when offset doesn't match expected value", func() {
+				none := ValidationTest{Offset: 0}
+				validator.Struct(none, processing)
+				Expect(processing.ErrorsArray.HasErrors()).To(BeTrue())
+			})
+
+			It("gives meaningfull failure message", func() {
+				none := ValidationTest{Offset: 0}
+				validator.Struct(none, processing)
+				Expect(processing.ErrorsArray.HasErrors()).To(BeTrue())
+				Expect(processing.ErrorsArray.Errors[0].Detail).To(ContainSubstring("'Offset' failed with 'Bad offset sorry' when given '0' for type 'int'"))
+			})
 		})
-		/*It("fails field validation", func() {
-			none := ValidationTest{Offset: 0}
-			Expect(validator.Field(none.Offset, "custom")).ToNot(BeNil())
-		})
-		It("fails field validation gives meaningfull message", func() {
-			none := ValidationTest{Offset: 99}
-			errs := validator.Field(none.Offset, "custom")
-			if errs != nil {
-				Expect(errs.GetError(validationFailureReasons).Error()).To(Equal("Field validation failed with 'Bad offset sorry' when given '99' for type 'int'"))
-			}
-		})
-		It("passes field validation", func() {
-			good := ValidationTest{Offset: 5}
-			Expect(validator.Field(good.Offset, "custom")).To(BeNil())
-		})*/
+
 	})
 
 })
