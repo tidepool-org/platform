6015f14eff289ce17a0bafef6b592d25f0538fae
Jamie <jamie@tidepool.org>
2016-04-05 19:46:49 +1200

    expanded device event with validation for all subtypes

--- data/deviceevent_test.go
+++ data/deviceevent_test.go
@@ -9,21 +9,107 @@ import (
 
 var _ = Describe("DeviceEvent", func() {
 
-	var deviceEventObj = testingDatumBase()
-	deviceEventObj["type"] = "deviceEvent"
-	deviceEventObj["subType"] = "alarm"
+	var processing validate.ErrorProcessing
 
-	var processing = validate.ErrorProcessing{BasePath: "0/deviceEvent", ErrorsArray: validate.NewErrorsArray()}
+	Context("calibration", func() {
 
-	Context("can be built from obj", func() {
-		It("should return a basal if the obj is valid", func() {
+		var deviceEventObj = testingDatumBase()
+		deviceEventObj["type"] = "deviceEvent"
+		deviceEventObj["subType"] = "calibration"
+		deviceEventObj["value"] = 3.0
+		deviceEventObj["units"] = "mg/dL"
+
+		It("returns a CalibrationDeviceEvent if the obj is valid", func() {
 			deviceEvent := BuildDeviceEvent(deviceEventObj, processing)
-			var deviceEventType *DeviceEvent
+			var deviceEventType *CalibrationDeviceEvent
 			Expect(deviceEvent).To(BeAssignableToTypeOf(deviceEventType))
-		})
-		It("should produce no error when valid", func() {
-			BuildDeviceEvent(deviceEventObj, processing)
 			Expect(processing.HasErrors()).To(BeFalse())
 		})
+
+		Context("validation", func() {})
+	})
+	Context("status", func() {
+
+		BeforeEach(func() {
+			processing = validate.ErrorProcessing{BasePath: "0/deviceEvent", ErrorsArray: validate.NewErrorsArray()}
+		})
+
+		var deviceEventObj = testingDatumBase()
+		deviceEventObj["type"] = "deviceEvent"
+		deviceEventObj["subType"] = "status"
+		deviceEventObj["status"] = "suspended"
+		deviceEventObj["reason"] = map[string]string{"suspended": "automatic"}
+		It("returns a StatusDeviceEvent if the obj is valid", func() {
+			deviceEvent := BuildDeviceEvent(deviceEventObj, processing)
+			var deviceEventType *StatusDeviceEvent
+			Expect(deviceEvent).To(BeAssignableToTypeOf(deviceEventType))
+			Expect(processing.HasErrors()).To(BeFalse())
+		})
+
+		Context("validation", func() {})
+	})
+	Context("alarm", func() {
+		BeforeEach(func() {
+			processing = validate.ErrorProcessing{BasePath: "0/deviceEvent", ErrorsArray: validate.NewErrorsArray()}
+		})
+		var deviceEventObj = testingDatumBase()
+		deviceEventObj["type"] = "deviceEvent"
+		deviceEventObj["subType"] = "alarm"
+		deviceEventObj["alarmType"] = "low_insulin"
+
+		It("returns a AlarmDeviceEvent if the obj is valid", func() {
+			deviceEvent := BuildDeviceEvent(deviceEventObj, processing)
+			var deviceEventType *AlarmDeviceEvent
+			Expect(deviceEvent).To(BeAssignableToTypeOf(deviceEventType))
+			Expect(processing.HasErrors()).To(BeFalse())
+		})
+
+		Context("validation", func() {})
+	})
+	Context("prime", func() {
+
+		BeforeEach(func() {
+			processing = validate.ErrorProcessing{BasePath: "0/deviceEvent", ErrorsArray: validate.NewErrorsArray()}
+		})
+
+		var deviceEventObj = testingDatumBase()
+		deviceEventObj["type"] = "deviceEvent"
+		deviceEventObj["subType"] = "prime"
+		deviceEventObj["primeTarget"] = "cannula"
+
+		It("returns a PrimeDeviceEvent if the obj is valid", func() {
+			deviceEvent := BuildDeviceEvent(deviceEventObj, processing)
+			var deviceEventType *PrimeDeviceEvent
+			Expect(deviceEvent).To(BeAssignableToTypeOf(deviceEventType))
+			Expect(processing.HasErrors()).To(BeFalse())
+		})
+
+		Context("validation", func() {})
+	})
+	Context("timeChange", func() {
+
+		BeforeEach(func() {
+			processing = validate.ErrorProcessing{BasePath: "0/deviceEvent", ErrorsArray: validate.NewErrorsArray()}
+		})
+
+		var deviceEventObj = testingDatumBase()
+		deviceEventObj["type"] = "deviceEvent"
+		deviceEventObj["subType"] = "timeChange"
+		deviceEventObj["change"] = map[string]interface{}{
+			"from":     "2015-03-08T12:02:00",
+			"to":       "2015-03-08T13:00:00",
+			"agent":    "manual",
+			"reasons":  []string{"to_daylight_savings", "correction"},
+			"timezone": "US/Pacific",
+		}
+
+		It("returns a TimeChangeDeviceEvent if the obj is valid", func() {
+			deviceEvent := BuildDeviceEvent(deviceEventObj, processing)
+			var deviceEventType *TimeChangeDeviceEvent
+			Expect(deviceEvent).To(BeAssignableToTypeOf(deviceEventType))
+			Expect(processing.HasErrors()).To(BeFalse())
+		})
+
+		Context("validation", func() {})
 	})
 })
