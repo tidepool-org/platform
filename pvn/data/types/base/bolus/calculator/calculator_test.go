5fdf372008c1523a4a78f82e249a5dc217fff64f
Jamie <jamie@tidepool.org>
2016-05-17 15:27:23 +1200

    tests for bgTarget and recommended

--- pvn/data/types/base/bolus/calculator/calculator_test.go
+++ pvn/data/types/base/bolus/calculator/calculator_test.go
@@ -23,6 +23,9 @@ var _ = Describe("Calculator Bolus", func() {
 		rawObject["insulinCarbRatio"] = 50
 		rawObject["insulinOnBoard"] = 70
 
+		rawObject["recommended"] = map[string]interface{}{"net": 50, "correction": -50, "carb": 50}
+		rawObject["bgTarget"] = map[string]interface{}{"target": 100, "range": 10}
+
 	})
 
 	Context("insulinOnBoard", func() {
@@ -53,7 +56,7 @@ var _ = Describe("Calculator Bolus", func() {
 
 	Context("units", func() {
 
-		DescribeTable("units when", testing.ExpectFieldNotValid,
+		DescribeTable("invalid when", testing.ExpectFieldNotValid,
 			Entry("empty", rawObject, "units", "", []*service.Error{validator.ErrorValueNotTrue()}),
 			Entry("not one of the predefined values", rawObject, "units", "wrong", []*service.Error{validator.ErrorValueNotTrue()}),
 		)
@@ -69,7 +72,7 @@ var _ = Describe("Calculator Bolus", func() {
 
 	Context("bgInput", func() {
 
-		DescribeTable("value when", testing.ExpectFieldNotValid,
+		DescribeTable("invalid when", testing.ExpectFieldNotValid,
 			Entry("less than 0", rawObject, "bgInput", -0.1, []*service.Error{validator.ErrorValueNotTrue()}),
 			Entry("greater than 1000", rawObject, "bgInput", 1000.1, []*service.Error{validator.ErrorValueNotTrue()}),
 		)
@@ -85,7 +88,7 @@ var _ = Describe("Calculator Bolus", func() {
 
 	Context("insulinSensitivity", func() {
 
-		DescribeTable("value when", testing.ExpectFieldNotValid,
+		DescribeTable("invalid when", testing.ExpectFieldNotValid,
 			Entry("less than 0", rawObject, "insulinSensitivity", -0.1, []*service.Error{validator.ErrorValueNotTrue()}),
 			Entry("greater than 1000", rawObject, "insulinSensitivity", 1000.1, []*service.Error{validator.ErrorValueNotTrue()}),
 		)
@@ -101,7 +104,7 @@ var _ = Describe("Calculator Bolus", func() {
 
 	Context("carbInput", func() {
 
-		DescribeTable("value when", testing.ExpectFieldNotValid,
+		DescribeTable("invalid when", testing.ExpectFieldNotValid,
 			Entry("less than 0", rawObject, "carbInput", -1, []*service.Error{validator.ErrorValueNotTrue()}),
 			Entry("greater than 1000", rawObject, "carbInput", 1001, []*service.Error{validator.ErrorValueNotTrue()}),
 		)
@@ -110,6 +113,46 @@ var _ = Describe("Calculator Bolus", func() {
 			Entry("0", rawObject, "carbInput", 0),
 			Entry("in range", rawObject, "carbInput", 250),
 			Entry("below 1000", rawObject, "carbInput", 999),
+		)
+
+	})
+
+	Context("bgTarget", func() {
+
+		DescribeTable("invalid when", testing.ExpectFieldNotValid,
+			Entry("range less than 0", rawObject, "bgTarget", map[string]interface{}{"target": 100, "range": -1}, []*service.Error{validator.ErrorValueNotTrue()}),
+			Entry("range greater than 50", rawObject, "bgTarget", map[string]interface{}{"target": 100, "range": 51}, []*service.Error{validator.ErrorValueNotTrue()}),
+			Entry("target less than 0", rawObject, "bgTarget", map[string]interface{}{"target": -0.1, "range": 10}, []*service.Error{validator.ErrorValueNotTrue()}),
+			Entry("target greater than 1000", rawObject, "bgTarget", map[string]interface{}{"target": 1000.1, "range": 10}, []*service.Error{validator.ErrorValueNotTrue()}),
+		)
+
+		DescribeTable("valid when", testing.ExpectFieldIsValid,
+			Entry("range 0", rawObject, "bgTarget", map[string]interface{}{"target": 100, "range": 0}),
+			Entry("target 0", rawObject, "bgTarget", map[string]interface{}{"target": 0.0, "range": 10}),
+			Entry("range less or equal to 50", rawObject, "bgTarget", map[string]interface{}{"target": 100, "range": 50}),
+			Entry("target less or equal to 1000.0", rawObject, "bgTarget", map[string]interface{}{"target": 1000.0, "range": 10}),
+		)
+
+	})
+
+	Context("recommended", func() {
+
+		DescribeTable("invalid when", testing.ExpectFieldNotValid,
+			Entry("net less than -100", rawObject, "recommended", map[string]interface{}{"net": -101, "correction": -50, "carb": 50}, []*service.Error{validator.ErrorValueNotTrue()}),
+			Entry("net greater than 100", rawObject, "recommended", map[string]interface{}{"net": 101, "correction": -50, "carb": 50}, []*service.Error{validator.ErrorValueNotTrue()}),
+			Entry("correction less than -100", rawObject, "recommended", map[string]interface{}{"net": 50, "correction": -101, "carb": 50}, []*service.Error{validator.ErrorValueNotTrue()}),
+			Entry("correction greater than 100", rawObject, "recommended", map[string]interface{}{"net": 50, "correction": 101, "carb": 50}, []*service.Error{validator.ErrorValueNotTrue()}),
+			Entry("carb less than 0", rawObject, "recommended", map[string]interface{}{"net": 50, "correction": -50, "carb": -1}, []*service.Error{validator.ErrorValueNotTrue()}),
+			Entry("carb greater than 100", rawObject, "recommended", map[string]interface{}{"net": 50, "correction": -50, "carb": 101}, []*service.Error{validator.ErrorValueNotTrue()}),
+		)
+
+		DescribeTable("valid when", testing.ExpectFieldIsValid,
+			Entry("net more or equal -100", rawObject, "recommended", map[string]interface{}{"net": -100, "correction": -50, "carb": 50}),
+			Entry("net less or equal 100", rawObject, "recommended", map[string]interface{}{"net": 100, "correction": -50, "carb": 50}),
+			Entry("correction more or equal -100", rawObject, "recommended", map[string]interface{}{"net": 10, "correction": -100, "carb": 50}),
+			Entry("correction less or equal 100", rawObject, "recommended", map[string]interface{}{"net": 10, "correction": 100, "carb": 50}),
+			Entry("carb more or equal 0", rawObject, "recommended", map[string]interface{}{"net": -100, "correction": -50, "carb": 0}),
+			Entry("carb less or equal 50", rawObject, "recommended", map[string]interface{}{"net": 100, "correction": -50, "carb": 50}),
 		)
 
 	})
