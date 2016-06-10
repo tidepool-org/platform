da559831274a599187589a441388375b98683b15
Darin Krauss <darinkrauss@gmail.com>
2016-06-09 17:26:39 -0700

    Upload DataState and Deduplicator are private

--- data/types/base/upload/upload.go
+++ data/types/base/upload/upload.go
@@ -9,9 +9,9 @@ import (
 type Upload struct {
 	base.Base `bson:",inline"`
 
-	DataState    string      `json:"dataState,omitempty" bson:"dataState,omitempty"`
+	DataState    string      `json:"-" bson:"_dataState,omitempty"`
+	Deduplicator interface{} `json:"-" bson:"_deduplicator,omitempty"`
 	UploadUserID string      `json:"byUser,omitempty" bson:"byUser,omitempty"`
-	Deduplicator interface{} `json:"deduplicator,omitempty" bson:"deduplicator,omitempty"`
 
 	ComputerTime        *string   `json:"computerTime,omitempty" bson:"computerTime,omitempty"`
 	DeviceManufacturers *[]string `json:"deviceManufacturers,omitempty" bson:"deviceManufacturers,omitempty"`
@@ -48,7 +48,6 @@ func (u *Upload) Parse(parser data.ObjectParser) error {
 		return err
 	}
 
-	// u.UploadUserID = parser.ParseString("byUser") // TODO_DATA: Do not parse, we set this
 	u.Version = parser.ParseString("version")
 	u.ComputerTime = parser.ParseString("computerTime")
 	u.DeviceTags = parser.ParseStringArray("deviceTags")
@@ -57,8 +56,6 @@ func (u *Upload) Parse(parser data.ObjectParser) error {
 	u.DeviceSerialNumber = parser.ParseString("deviceSerialNumber")
 	u.TimeProcessing = parser.ParseString("timeProcessing")
 	u.TimeZone = parser.ParseString("timezone")
-	// u.DataState = parser.ParseString("dataState") // TODO_DATA: Do not parse, we set this
-	// u.Deduplicator = parser.ParseInterface("deduplicator") // TODO_DATA: Do not parse, we set this
 
 	return nil
 }
@@ -70,8 +67,6 @@ func (u *Upload) Validate(validator data.Validator) error {
 		return err
 	}
 
-	// validator.ValidateString("type", u.Type).Exists() // TODO_DATA: Already done in Base
-	// validator.ValidateString("byUser", u.UploadUserID).Exists().LengthGreaterThanOrEqualTo(10) // TODO_DATA: Validation is for parsed data only
 	validator.ValidateString("version", u.Version).Exists().LengthGreaterThan(5)
 	validator.ValidateStringAsTime("computerTime", u.ComputerTime, "2006-01-02T15:04:05").Exists()
 	validator.ValidateStringArray("deviceTags", u.DeviceTags).Exists().NotEmpty().EachOneOf([]string{"insulin-pump", "cgm", "bgm"})
@@ -80,8 +75,6 @@ func (u *Upload) Validate(validator data.Validator) error {
 	validator.ValidateString("deviceSerialNumber", u.DeviceSerialNumber).Exists().LengthGreaterThan(1)
 	validator.ValidateString("timeProcessing", u.TimeProcessing).Exists().OneOf([]string{"across-the-board-timezone", "utc-bootstrapping", "none"})
 	validator.ValidateString("timezone", u.TimeZone).Exists().LengthGreaterThan(1)
-	// validator.ValidateString("dataState", u.DataState).Exists().LengthGreaterThan(1) // TODO_DATA: Validation is for parsed data only
-	// validator.ValidateInterface("deduplicator", u.Deduplicator).Exists() // TODO_DATA: Validation is for parsed data only
 
 	return nil
 }
