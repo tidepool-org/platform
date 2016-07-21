2f02162927f55aacaf4d9cc4e987cd29187e9247
Darin Krauss <darinkrauss@gmail.com>
2016-07-21 16:50:04 -0700

    Minor documentation cleanup

--- dataservices/server/api.go
+++ dataservices/server/api.go
@@ -1,5 +1,15 @@
 package server
 
+/* CHECKLIST
+ * [x] Uses interfaces as appropriate
+ * [x] Private package variables use underscore prefix
+ * [x] All parameters validated
+ * [x] All errors handled
+ * [x] Reviewed for concurrency safety
+ * [x] Code complete
+ * [x] Full test coverage
+ */
+
 import "net/http"
 
 type API interface {
--- dataservices/server/context.go
+++ dataservices/server/context.go
@@ -1,5 +1,15 @@
 package server
 
+/* CHECKLIST
+ * [x] Uses interfaces as appropriate
+ * [x] Private package variables use underscore prefix
+ * [x] All parameters validated
+ * [x] All errors handled
+ * [x] Reviewed for concurrency safety
+ * [x] Code complete
+ * [x] Full test coverage
+ */
+
 import (
 	"github.com/tidepool-org/platform/data"
 	"github.com/tidepool-org/platform/data/deduplicator"
