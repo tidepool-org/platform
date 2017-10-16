f76d2ba5da40e53dd68dac82b0856b6e04dbff1e
Darin Krauss <darin@tidepool.org>
2017-10-16 15:10:53 -0700

    Service tests

--- service/request_test.go
+++ service/request_test.go
@@ -1,301 +1,293 @@
 package service_test
 
-// import (
-// 	. "github.com/onsi/ginkgo"
-// 	. "github.com/onsi/gomega"
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
-// 	"net/http"
+	"net/http"
 
-// 	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/ant0ine/go-json-rest/rest"
 
-// 	testAuth "github.com/tidepool-org/platform/auth/test"
-// 	"github.com/tidepool-org/platform/log"
-// 	"github.com/tidepool-org/platform/log/null"
-// 	"github.com/tidepool-org/platform/service"
-// )
+	testAuth "github.com/tidepool-org/platform/auth/test"
+	"github.com/tidepool-org/platform/id"
+	"github.com/tidepool-org/platform/log"
+	logNull "github.com/tidepool-org/platform/log/null"
+	"github.com/tidepool-org/platform/request"
+	"github.com/tidepool-org/platform/service"
+	testRest "github.com/tidepool-org/platform/test/rest"
+)
 
-// func NewTestRequest() *rest.Request {
-// 	baseRequest, err := http.NewRequest("GET", "http://127.0.0.1/", nil)
-// 	Expect(err).ToNot(HaveOccurred())
-// 	Expect(baseRequest).ToNot(BeNil())
-// 	return &rest.Request{
-// 		Request:    baseRequest,
-// 		PathParams: map[string]string{},
-// 		Env:        map[string]interface{}{},
-// 	}
-// }
+var _ = Describe("Request", func() {
+	Context("with request", func() {
+		var errs []*service.Error
+		var logger log.Logger
+		var details request.Details
+		var req *rest.Request
 
-// var _ = Describe("Request", func() {
-// 	Context("with request", func() {
-// 		var errors []*service.Error
-// 		var logger log.Logger
-// 		var authDetails *testAuth.Details
-// 		var request *rest.Request
+		BeforeEach(func() {
+			errs = []*service.Error{
+				{
+					Code:   "test-error-code",
+					Status: 400,
+					Title:  "test-error-title",
+					Detail: "test-error-detail",
+				},
+			}
+			logger = logNull.NewLogger()
+			details = request.NewDetails(request.MethodSessionToken, id.New(), testAuth.NewSessionToken())
+			req = testRest.NewRequest()
+			req.Env["ERRORS"] = errs
+			req.Env["LOGGER"] = logger
+			req.Env["AUTH-DETAILS"] = details
+			req.Env["TRACE-REQUEST"] = "request-raven"
+			req.Env["TRACE-SESSION"] = "session-starling"
+		})
 
-// 		BeforeEach(func() {
-// 			errors = []*service.Error{
-// 				{
-// 					Code:   "test-error-code",
-// 					Status: 400,
-// 					Title:  "test-error-title",
-// 					Detail: "test-error-detail",
-// 				},
-// 			}
-// 			logger = null.NewLogger()
-// 			authDetails = testAuth.NewDetails()
-// 			request = NewTestRequest()
-// 			request.Env["ERRORS"] = errors
-// 			request.Env["LOGGER"] = logger
-// 			request.Env["AUTH-DETAILS"] = authDetails
-// 			request.Env["TRACE-REQUEST"] = "request-raven"
-// 			request.Env["TRACE-SESSION"] = "session-starling"
-// 		})
+		Context("GetRequestErrors", func() {
+			It("returns successfully", func() {
+				Expect(service.GetRequestErrors(req)).To(Equal(errs))
+			})
 
-// 		Context("GetRequestErrors", func() {
-// 			It("returns successfully", func() {
-// 				Expect(service.GetRequestErrors(request)).To(Equal(errors))
-// 			})
+			It("returns nil if the request is missing", func() {
+				Expect(service.GetRequestErrors(nil)).To(BeNil())
+			})
 
-// 			It("returns nil if the request is missing", func() {
-// 				Expect(service.GetRequestErrors(nil)).To(BeNil())
-// 			})
+			It("returns nil if the errors is not of the correct type", func() {
+				req.Env["ERRORS"] = 0
+				Expect(service.GetRequestErrors(req)).To(BeNil())
+			})
 
-// 			It("returns nil if the errors is not of the correct type", func() {
-// 				request.Env["ERRORS"] = 0
-// 				Expect(service.GetRequestErrors(request)).To(BeNil())
-// 			})
+			It("returns nil if the errors is missing", func() {
+				delete(req.Env, "ERRORS")
+				Expect(service.GetRequestErrors(req)).To(BeNil())
+			})
+		})
 
-// 			It("returns nil if the errors is missing", func() {
-// 				delete(request.Env, "ERRORS")
-// 				Expect(service.GetRequestErrors(request)).To(BeNil())
-// 			})
-// 		})
+		Context("SetRequestErrors", func() {
+			var newErrs []*service.Error
 
-// 		Context("SetRequestErrors", func() {
-// 			var newErrors []*service.Error
+			BeforeEach(func() {
+				newErrs = []*service.Error{
+					{
+						Code:   "test-error-code-new",
+						Status: 400,
+						Title:  "test-error-title-new",
+						Detail: "test-error-detail-new",
+					},
+				}
+			})
 
-// 			BeforeEach(func() {
-// 				newErrors = []*service.Error{
-// 					{
-// 						Code:   "test-error-code-new",
-// 						Status: 400,
-// 						Title:  "test-error-title-new",
-// 						Detail: "test-error-detail-new",
-// 					},
-// 				}
-// 			})
+			It("successfully sets the errors", func() {
+				service.SetRequestErrors(req, newErrs)
+				Expect(req.Env["ERRORS"]).To(Equal(newErrs))
+			})
 
-// 			It("successfully sets the errors", func() {
-// 				service.SetRequestErrors(request, newErrors)
-// 				Expect(request.Env["ERRORS"]).To(Equal(newErrors))
-// 			})
+			It("does nothing if the request is missing", func() {
+				service.SetRequestErrors(nil, newErrs)
+				Expect(req.Env["ERRORS"]).To(Equal(errs))
+			})
 
-// 			It("does nothing if the request is missing", func() {
-// 				service.SetRequestErrors(nil, newErrors)
-// 				Expect(request.Env["ERRORS"]).To(Equal(errors))
-// 			})
+			It("deletes the errors if the errors is missing", func() {
+				service.SetRequestErrors(req, nil)
+				Expect(req.Env["ERRORS"]).To(BeNil())
+			})
+		})
 
-// 			It("deletes the errors if the errors is missing", func() {
-// 				service.SetRequestErrors(request, nil)
-// 				Expect(request.Env["ERRORS"]).To(BeNil())
-// 			})
-// 		})
+		Context("GetRequestLogger", func() {
+			It("returns successfully", func() {
+				Expect(service.GetRequestLogger(req)).To(Equal(logger))
+			})
 
-// 		Context("GetRequestLogger", func() {
-// 			It("returns successfully", func() {
-// 				Expect(service.GetRequestLogger(request)).To(Equal(logger))
-// 			})
+			It("returns nil if the request is missing", func() {
+				Expect(service.GetRequestLogger(nil)).To(BeNil())
+			})
 
-// 			It("returns nil if the request is missing", func() {
-// 				Expect(service.GetRequestLogger(nil)).To(BeNil())
-// 			})
+			It("returns nil if the logger is not of the correct type", func() {
+				req.Env["LOGGER"] = 0
+				Expect(service.GetRequestLogger(req)).To(BeNil())
+			})
 
-// 			It("returns nil if the logger is not of the correct type", func() {
-// 				request.Env["LOGGER"] = 0
-// 				Expect(service.GetRequestLogger(request)).To(BeNil())
-// 			})
+			It("returns nil if the logger is missing", func() {
+				delete(req.Env, "LOGGER")
+				Expect(service.GetRequestLogger(req)).To(BeNil())
+			})
+		})
 
-// 			It("returns nil if the logger is missing", func() {
-// 				delete(request.Env, "LOGGER")
-// 				Expect(service.GetRequestLogger(request)).To(BeNil())
-// 			})
-// 		})
+		Context("SetRequestLogger", func() {
+			var newLogger log.Logger
 
-// 		Context("SetRequestLogger", func() {
-// 			var newLogger log.Logger
+			BeforeEach(func() {
+				newLogger = logNull.NewLogger()
+			})
 
-// 			BeforeEach(func() {
-// 				newLogger = null.NewLogger()
-// 			})
+			It("successfully sets the logger", func() {
+				service.SetRequestLogger(req, newLogger)
+				Expect(req.Env["LOGGER"]).To(Equal(newLogger))
+			})
 
-// 			It("successfully sets the logger", func() {
-// 				service.SetRequestLogger(request, newLogger)
-// 				Expect(request.Env["LOGGER"]).To(Equal(newLogger))
-// 			})
+			It("does nothing if the request is missing", func() {
+				service.SetRequestLogger(nil, newLogger)
+				Expect(req.Env["LOGGER"]).To(Equal(logger))
+			})
 
-// 			It("does nothing if the request is missing", func() {
-// 				service.SetRequestLogger(nil, newLogger)
-// 				Expect(request.Env["LOGGER"]).To(Equal(logger))
-// 			})
+			It("deletes the logger if the logger is missing", func() {
+				service.SetRequestLogger(req, nil)
+				Expect(req.Env["LOGGER"]).To(BeNil())
+			})
+		})
 
-// 			It("deletes the logger if the logger is missing", func() {
-// 				service.SetRequestLogger(request, nil)
-// 				Expect(request.Env["LOGGER"]).To(BeNil())
-// 			})
-// 		})
+		Context("GetRequestAuthDetails", func() {
+			It("returns successfully", func() {
+				Expect(service.GetRequestAuthDetails(req)).To(Equal(details))
+			})
 
-// 		Context("GetRequestAuthDetails", func() {
-// 			It("returns successfully", func() {
-// 				Expect(service.GetRequestAuthDetails(request)).To(Equal(authDetails))
-// 			})
+			It("returns nil if the request is missing", func() {
+				Expect(service.GetRequestAuthDetails(nil)).To(BeNil())
+			})
 
-// 			It("returns nil if the request is missing", func() {
-// 				Expect(service.GetRequestAuthDetails(nil)).To(BeNil())
-// 			})
+			It("returns nil if the auth details is not of the correct type", func() {
+				req.Env["AUTH-DETAILS"] = 0
+				Expect(service.GetRequestAuthDetails(req)).To(BeNil())
+			})
 
-// 			It("returns nil if the auth details is not of the correct type", func() {
-// 				request.Env["AUTH-DETAILS"] = 0
-// 				Expect(service.GetRequestAuthDetails(request)).To(BeNil())
-// 			})
+			It("returns nil if the auth details is missing", func() {
+				delete(req.Env, "AUTH-DETAILS")
+				Expect(service.GetRequestAuthDetails(req)).To(BeNil())
+			})
+		})
 
-// 			It("returns nil if the auth details is missing", func() {
-// 				delete(request.Env, "AUTH-DETAILS")
-// 				Expect(service.GetRequestAuthDetails(request)).To(BeNil())
-// 			})
-// 		})
+		Context("SetRequestAuthDetails", func() {
+			var newDetails request.Details
 
-// 		Context("SetRequestAuthDetails", func() {
-// 			var newAuthDetails *testAuth.Details
+			BeforeEach(func() {
+				newDetails = request.NewDetails(request.MethodSessionToken, id.New(), testAuth.NewSessionToken())
+			})
 
-// 			BeforeEach(func() {
-// 				newAuthDetails = testAuth.NewDetails()
-// 			})
+			It("successfully sets the auth details", func() {
+				service.SetRequestAuthDetails(req, newDetails)
+				Expect(req.Env["AUTH-DETAILS"]).To(Equal(newDetails))
+			})
 
-// 			It("successfully sets the auth details", func() {
-// 				service.SetRequestAuthDetails(request, newAuthDetails)
-// 				Expect(request.Env["AUTH-DETAILS"]).To(Equal(newAuthDetails))
-// 			})
+			It("does nothing if the request is missing", func() {
+				service.SetRequestAuthDetails(nil, newDetails)
+				Expect(req.Env["AUTH-DETAILS"]).To(Equal(details))
+			})
 
-// 			It("does nothing if the request is missing", func() {
-// 				service.SetRequestAuthDetails(nil, newAuthDetails)
-// 				Expect(request.Env["AUTH-DETAILS"]).To(Equal(authDetails))
-// 			})
+			It("deletes the auth details if the auth details is missing", func() {
+				service.SetRequestAuthDetails(req, nil)
+				Expect(req.Env["AUTH-DETAILS"]).To(BeNil())
+			})
+		})
 
-// 			It("deletes the auth details if the auth details is missing", func() {
-// 				service.SetRequestAuthDetails(request, nil)
-// 				Expect(request.Env["AUTH-DETAILS"]).To(BeNil())
-// 			})
-// 		})
+		Context("GetRequestTraceRequest", func() {
+			It("returns successfully", func() {
+				Expect(service.GetRequestTraceRequest(req)).To(Equal("request-raven"))
+			})
 
-// 		Context("GetRequestTraceRequest", func() {
-// 			It("returns successfully", func() {
-// 				Expect(service.GetRequestTraceRequest(request)).To(Equal("request-raven"))
-// 			})
+			It("returns empty string if the request is missing", func() {
+				Expect(service.GetRequestTraceRequest(nil)).To(BeEmpty())
+			})
 
-// 			It("returns empty string if the request is missing", func() {
-// 				Expect(service.GetRequestTraceRequest(nil)).To(BeEmpty())
-// 			})
+			It("returns empty string if the request trace is not of the correct type", func() {
+				req.Env["TRACE-REQUEST"] = 0
+				Expect(service.GetRequestTraceRequest(req)).To(BeEmpty())
+			})
 
-// 			It("returns empty string if the request trace is not of the correct type", func() {
-// 				request.Env["TRACE-REQUEST"] = 0
-// 				Expect(service.GetRequestTraceRequest(request)).To(BeEmpty())
-// 			})
+			It("returns empty string if the request trace is missing", func() {
+				delete(req.Env, "TRACE-REQUEST")
+				Expect(service.GetRequestTraceRequest(req)).To(BeEmpty())
+			})
+		})
 
-// 			It("returns empty string if the request trace is missing", func() {
-// 				delete(request.Env, "TRACE-REQUEST")
-// 				Expect(service.GetRequestTraceRequest(request)).To(BeEmpty())
-// 			})
-// 		})
+		Context("SetRequestTraceRequest", func() {
+			It("successfully sets the request trace", func() {
+				service.SetRequestTraceRequest(req, "request-raven-new")
+				Expect(req.Env["TRACE-REQUEST"]).To(Equal("request-raven-new"))
+			})
 
-// 		Context("SetRequestTraceRequest", func() {
-// 			It("successfully sets the request trace", func() {
-// 				service.SetRequestTraceRequest(request, "request-raven-new")
-// 				Expect(request.Env["TRACE-REQUEST"]).To(Equal("request-raven-new"))
-// 			})
+			It("does nothing if the request is missing", func() {
+				service.SetRequestTraceRequest(nil, "request-raven-new")
+				Expect(req.Env["TRACE-REQUEST"]).To(Equal("request-raven"))
+			})
 
-// 			It("does nothing if the request is missing", func() {
-// 				service.SetRequestTraceRequest(nil, "request-raven-new")
-// 				Expect(request.Env["TRACE-REQUEST"]).To(Equal("request-raven"))
-// 			})
+			It("deletes the request trace if the request trace is missing", func() {
+				service.SetRequestTraceRequest(req, "")
+				Expect(req.Env["TRACE-REQUEST"]).To(BeNil())
+			})
+		})
 
-// 			It("deletes the request trace if the request trace is missing", func() {
-// 				service.SetRequestTraceRequest(request, "")
-// 				Expect(request.Env["TRACE-REQUEST"]).To(BeNil())
-// 			})
-// 		})
+		Context("GetRequestTraceSession", func() {
+			It("returns successfully", func() {
+				Expect(service.GetRequestTraceSession(req)).To(Equal("session-starling"))
+			})
 
-// 		Context("GetRequestTraceSession", func() {
-// 			It("returns successfully", func() {
-// 				Expect(service.GetRequestTraceSession(request)).To(Equal("session-starling"))
-// 			})
+			It("returns empty string if the request is missing", func() {
+				Expect(service.GetRequestTraceSession(nil)).To(BeEmpty())
+			})
 
-// 			It("returns empty string if the request is missing", func() {
-// 				Expect(service.GetRequestTraceSession(nil)).To(BeEmpty())
-// 			})
+			It("returns empty string if the session trace is not of the correct type", func() {
+				req.Env["TRACE-SESSION"] = 0
+				Expect(service.GetRequestTraceSession(req)).To(BeEmpty())
+			})
 
-// 			It("returns empty string if the session trace is not of the correct type", func() {
-// 				request.Env["TRACE-SESSION"] = 0
-// 				Expect(service.GetRequestTraceSession(request)).To(BeEmpty())
-// 			})
+			It("returns empty string if the session trace is missing", func() {
+				delete(req.Env, "TRACE-SESSION")
+				Expect(service.GetRequestTraceSession(req)).To(BeEmpty())
+			})
+		})
 
-// 			It("returns empty string if the session trace is missing", func() {
-// 				delete(request.Env, "TRACE-SESSION")
-// 				Expect(service.GetRequestTraceSession(request)).To(BeEmpty())
-// 			})
-// 		})
+		Context("SetRequestTraceSession", func() {
+			It("successfully sets the session trace", func() {
+				service.SetRequestTraceSession(req, "session-starling-new")
+				Expect(req.Env["TRACE-SESSION"]).To(Equal("session-starling-new"))
+			})
 
-// 		Context("SetRequestTraceSession", func() {
-// 			It("successfully sets the session trace", func() {
-// 				service.SetRequestTraceSession(request, "session-starling-new")
-// 				Expect(request.Env["TRACE-SESSION"]).To(Equal("session-starling-new"))
-// 			})
+			It("does nothing if the request is missing", func() {
+				service.SetRequestTraceSession(nil, "session-starling-new")
+				Expect(req.Env["TRACE-SESSION"]).To(Equal("session-starling"))
+			})
 
-// 			It("does nothing if the request is missing", func() {
-// 				service.SetRequestTraceSession(nil, "session-starling-new")
-// 				Expect(request.Env["TRACE-SESSION"]).To(Equal("session-starling"))
-// 			})
+			It("deletes the session trace if the session trace is missing", func() {
+				service.SetRequestTraceSession(req, "")
+				Expect(req.Env["TRACE-SESSION"]).To(BeNil())
+			})
+		})
 
-// 			It("deletes the session trace if the session trace is missing", func() {
-// 				service.SetRequestTraceSession(request, "")
-// 				Expect(request.Env["TRACE-SESSION"]).To(BeNil())
-// 			})
-// 		})
+		Context("CopyRequestTrace", func() {
+			var destinationRequest *http.Request
 
-// 		Context("CopyRequestTrace", func() {
-// 			var destinationRequest *http.Request
+			BeforeEach(func() {
+				var err error
+				destinationRequest, err = http.NewRequest("GET", "http://127.0.0.1/", nil)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(destinationRequest).ToNot(BeNil())
+			})
 
-// 			BeforeEach(func() {
-// 				var err error
-// 				destinationRequest, err = http.NewRequest("GET", "http://127.0.0.1/", nil)
-// 				Expect(err).ToNot(HaveOccurred())
-// 				Expect(destinationRequest).ToNot(BeNil())
-// 			})
+			It("is successful", func() {
+				Expect(service.CopyRequestTrace(req, destinationRequest)).To(Succeed())
+				Expect(destinationRequest.Header["X-Tidepool-Trace-Request"]).To(ConsistOf("request-raven"))
+				Expect(destinationRequest.Header["X-Tidepool-Trace-Session"]).To(ConsistOf("session-starling"))
+			})
 
-// 			It("is successful", func() {
-// 				Expect(service.CopyRequestTrace(request, destinationRequest)).To(Succeed())
-// 				Expect(destinationRequest.Header["X-Tidepool-Trace-Request"]).To(ConsistOf("request-raven"))
-// 				Expect(destinationRequest.Header["X-Tidepool-Trace-Session"]).To(ConsistOf("session-starling"))
-// 			})
+			It("returns an error if the source request is missing", func() {
+				Expect(service.CopyRequestTrace(nil, destinationRequest)).To(MatchError("source request is missing"))
+			})
 
-// 			It("returns an error if the source request is missing", func() {
-// 				Expect(service.CopyRequestTrace(nil, destinationRequest)).To(MatchError("source request is missing"))
-// 			})
+			It("returns an error if the destination request is missing", func() {
+				Expect(service.CopyRequestTrace(req, nil)).To(MatchError("destination request is missing"))
+			})
 
-// 			It("returns an error if the destination request is missing", func() {
-// 				Expect(service.CopyRequestTrace(request, nil)).To(MatchError("destination request is missing"))
-// 			})
+			It("is successful even if request trace not set", func() {
+				delete(req.Env, "TRACE-REQUEST")
+				Expect(service.CopyRequestTrace(req, destinationRequest)).To(Succeed())
+				Expect(destinationRequest.Header["X-Tidepool-Trace-Request"]).To(BeEmpty())
+			})
 
-// 			It("is successful even if request trace not set", func() {
-// 				delete(request.Env, "TRACE-REQUEST")
-// 				Expect(service.CopyRequestTrace(request, destinationRequest)).To(Succeed())
-// 				Expect(destinationRequest.Header["X-Tidepool-Trace-Request"]).To(BeEmpty())
-// 			})
-
-// 			It("is successful even if session trace not set", func() {
-// 				delete(request.Env, "TRACE-SESSION")
-// 				Expect(service.CopyRequestTrace(request, destinationRequest)).To(Succeed())
-// 				Expect(destinationRequest.Header["X-Tidepool-Trace-Session"]).To(BeEmpty())
-// 			})
-// 		})
-// 	})
-// })
+			It("is successful even if session trace not set", func() {
+				delete(req.Env, "TRACE-SESSION")
+				Expect(service.CopyRequestTrace(req, destinationRequest)).To(Succeed())
+				Expect(destinationRequest.Header["X-Tidepool-Trace-Session"]).To(BeEmpty())
+			})
+		})
+	})
+})
