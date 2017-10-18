047ff7c28ab148c9390538d8897b129fb38c77b6
Darin Krauss <darin@tidepool.org>
2017-10-17 18:21:29 -0700

    Replace two data service routes accidentally removed

--- data/service/api/v1/v1.go
+++ data/service/api/v1/v1.go
@@ -8,6 +8,8 @@ func Routes() []service.Route {
 		service.MakeRoute("DELETE", "/v1/datasets/:dataSetId", Authenticate(DatasetsDelete)),
 		service.MakeRoute("PUT", "/v1/datasets/:dataSetId", Authenticate(DatasetsUpdate)),
 		service.MakeRoute("DELETE", "/v1/users/:userId/data", Authenticate(UsersDataDelete)),
+		service.MakeRoute("POST", "/v1/users/:userId/datasets", Authenticate(UsersDatasetsCreate)),
+		service.MakeRoute("GET", "/v1/users/:userId/datasets", Authenticate(UsersDatasetsGet)),
 
 		service.MakeRoute("POST", "/v1/data_sets/:dataSetId/data", Authenticate(DatasetsDataCreate)),
 		service.MakeRoute("DELETE", "/v1/data_sets/:dataSetId", Authenticate(DatasetsDelete)),
