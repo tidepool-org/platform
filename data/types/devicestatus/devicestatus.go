9c3f9711be87601b2c811cc701157878144c8936
james raby <james@jamess-mbp.lan>
2019-09-09 17:03:29 -0700

    Start of Device Status incorporation into the model

---
+++ data/types/devicestatus/devicestatus.go
@@ -0,0 +1,51 @@
+package devicestatus
+
+import (
+	"github.com/tidepool-org/platform/data"
+	"github.com/tidepool-org/platform/data/types"
+	"github.com/tidepool-org/platform/data/types/devicestatus/status"
+	"github.com/tidepool-org/platform/structure"
+)
+
+const (
+	Eid      = "eid"
+	Cgm      = "cgm"
+	Pump     = "pump"
+	SmartPen = "smartpen"
+)
+
+func DeviceTypes() []string {
+	return []string{Eid, Cgm, Pump, SmartPen}
+}
+
+type DeviceStatus struct {
+	types.Base `bson:",inline"`
+
+	DeviceType *string      `json:"deviceType,omitempty" bson:"deviceType,omitempty"`
+	Version    *string      `json:"version,omitempty" bson:"version,omitempty"`
+	Status     status.Array `json:"status,omitempty" bson:"status,omitempty"`
+}
+
+func ParseDeviceStatus(parser structure.ObjectParser) *DeviceStatus {
+	if !parser.Exists() {
+		return nil
+	}
+	datum := NewDeviceStatus()
+	parser.Parse(datum)
+	return datum
+}
+
+func NewDeviceStatus() *DeviceStatus {
+	return &DeviceStatus{}
+}
+
+func (a *DeviceStatus) Parse(parser structure.ObjectParser) {
+	a.DeviceType = parser.String("deviceType")
+	a.Version = parser.String("version")
+}
+
+func (a *DeviceStatus) Validate(validator structure.Validator) {
+	validator.String("deviceType", a.DeviceType).Exists().OneOf(DeviceTypes()...)
+}
+
+func (a *DeviceStatus) Normalize(normalizer data.Normalizer) {}
---
+++ data/types/devicestatus/devicestatus_test.go
@@ -0,0 +1,42 @@
+package devicestatus_test
+
+import (
+	. "github.com/onsi/ginkgo"
+
+	"github.com/tidepool-org/platform/data/types/devicestatus"
+)
+
+func NewDeviceStatus() *devicestatus.DeviceStatus {
+	datum := devicestatus.NewDeviceStatus()
+	return datum
+}
+
+func CloneDeviceStatus(datum *devicestatus.DeviceStatus) *devicestatus.DeviceStatus {
+	if datum == nil {
+		return nil
+	}
+	clone := devicestatus.NewDeviceStatus()
+	return clone
+}
+
+var _ = Describe("DeviceStatus", func() {
+
+	Context("ParseDeviceStatus", func() {
+		// TODO
+	})
+
+	Context("NewDeviceStatus", func() {
+	})
+
+	Context("DeviceStatus", func() {
+		Context("Parse", func() {
+			// TODO
+		})
+
+		Context("Validate", func() {
+		})
+
+		Context("Normalize", func() {
+		})
+	})
+})
