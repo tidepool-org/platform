370706f04255391dce8ccd64dad5f0c9296ecc15
Todd Kazakov <todd.kazakov@gmail.com>
2023-03-15 15:43:41 +0200

    Add ability to disable index creation on startup with an env variable Allow disabling the user events handler Add error context Fix imports

--- blob/service/service.go
+++ blob/service/service.go
@@ -65,7 +65,7 @@ func (s *Service) Initialize(provider application.Provider) error {
 	if err := s.initializeBlobClient(); err != nil {
 		return err
 	}
-	if err := s.initializeUserEventsHandler(); err != nil {
+	if err := s.initializeUserEventsHandler(provider); err != nil {
 		return err
 	}
 	return s.initializeRouter()
@@ -174,13 +174,20 @@ func (s *Service) terminateBlobUnstructuredStore() {
 	}
 }
 
-func (s *Service) initializeUserEventsHandler() error {
+func (s *Service) initializeUserEventsHandler(provider application.Provider) error {
 	s.Logger().Debug("Initializing user events handler")
 
-	ctx := logInternal.NewContextWithLogger(context.Background(), s.Logger())
-	handler := blobEvents.NewUserDataDeletionHandler(ctx, s.blobClient)
-	handlers := []eventsCommon.EventHandler{handler}
-	runner := events.NewRunner(handlers)
+	var runner events.Runner
+
+	configReporter := provider.ConfigReporter().WithScopes("user", "events", "handler")
+	if configReporter.GetWithDefault("disable", "") != "true" {
+		ctx := logInternal.NewContextWithLogger(context.Background(), s.Logger())
+		handler := blobEvents.NewUserDataDeletionHandler(ctx, s.blobClient)
+		handlers := []eventsCommon.EventHandler{handler}
+		runner = events.NewRunner(handlers)
+	} else {
+		runner = events.NewNoopRunner()
+	}
 
 	if err := runner.Initialize(); err != nil {
 		return errors.Wrap(err, "unable to initialize events runner")
