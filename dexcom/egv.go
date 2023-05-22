773bcea49cbb1a83115d39612a1e7148381cba71
Jamie <jamie.h.bate@gmail.com>
2023-05-23 11:22:09 +1200

    stopgap for when no units sent

--- dexcom/egv.go
+++ dexcom/egv.go
@@ -5,6 +5,7 @@ import (
 
 	dataBloodGlucose "github.com/tidepool-org/platform/data/blood/glucose"
 	"github.com/tidepool-org/platform/data/types/settings/cgm"
+	"github.com/tidepool-org/platform/pointer"
 	"github.com/tidepool-org/platform/structure"
 	structureValidator "github.com/tidepool-org/platform/structure/validator"
 )
@@ -185,7 +186,13 @@ func (e *EGV) Parse(parser structure.ObjectParser) {
 	e.ID = parser.String("recordId")
 	e.SystemTime = TimeFromString(parser.String("systemTime"))
 	e.DisplayTime = TimeFromString(parser.String("displayTime"))
-	e.Unit = parser.String("unit")
+	// NOTE: currently we are finding that g7 data occasionally doesn't have the required units
+	// data. This is a workaround until that issue is resolved
+	unitVal := parser.String("unit")
+	if unitVal == nil {
+		unitVal = pointer.FromString(EGVUnitMgdL)
+	}
+	e.Unit = unitVal
 	e.RateUnit = parser.String("rateUnit")
 	e.Value = parser.Float64("value")
 	e.Status = parser.String("status")
