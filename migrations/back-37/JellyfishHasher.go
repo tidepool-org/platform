71712a6ee2550c4fca65d6043de4ee653711c727
Jamie <jamie.h.bate@gmail.com>
2023-09-07 19:17:45 +1200

    wip - jellyfish data migration

---
+++ migrations/back-37/JellyfishHasher.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/tidepool-org/platform/data/deduplicator/deduplicator"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func JellyfishIDHash(bsonData bson.M) string {
+	// represents data from base id fields
+	var baseData = func(bsonData bson.M) []string {
+		return []string{
+			bsonData["_userId"].(string),
+			bsonData["deviceId"].(string),
+			bsonData["time"].(string),
+			bsonData["type"].(string),
+		}
+	}
+	var theHash string
+	switch bsonData["type"] {
+	case "basal":
+		theHash = makeJellyfishHash(
+			append(baseData(bsonData), bsonData["deliveryType"].(string))...,
+		)
+	case "bolus":
+		theHash = makeJellyfishHash(
+			append(baseData(bsonData), bsonData["subType"].(string))...,
+		)
+	case "deviceEvent":
+		theHash = makeJellyfishHash(
+			append(baseData(bsonData), bsonData["subType"].(string))...,
+		)
+	case "smbg", "bloodKetone", "cbg":
+		theHash = makeJellyfishHash(
+			append(
+				baseData(bsonData),
+				bsonData["units"].(string),
+				strconv.FormatFloat(bsonData["value"].(float64), 'f', -1, 64),
+			)...,
+		)
+	default:
+		theHash = makeJellyfishHash(baseData(bsonData)...)
+	}
+	return theHash
+}
+
+func JellyfishObjectIDHash(bsonData bson.M) string {
+	return makeJellyfishHash(
+		JellyfishIDHash(bsonData),
+	)
+}
+
+func makeJellyfishHash(fields ...string) string {
+	hash, err := deduplicator.GenerateIdentityHash(fields)
+	if err != nil {
+		panic(err)
+	}
+	return hash
+}
---
+++ migrations/back-37/back-37.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/urfave/cli"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/tidepool-org/platform/application"
+	"github.com/tidepool-org/platform/data/deduplicator/deduplicator"
+	"github.com/tidepool-org/platform/errors"
+	migrationMongo "github.com/tidepool-org/platform/migration/mongo"
+	storeStructuredMongo "github.com/tidepool-org/platform/store/structured/mongo"
+)
+
+func main() {
+	application.RunAndExit(NewMigration())
+}
+
+type Migration struct {
+	*migrationMongo.Migration
+	dataRepository *storeStructuredMongo.Repository
+}
+
+func NewMigration() *Migration {
+	return &Migration{
+		Migration: migrationMongo.NewMigration(),
+	}
+}
+
+func (m *Migration) Initialize(provider application.Provider) error {
+	if err := m.Migration.Initialize(provider); err != nil {
+		return err
+	}
+
+	m.CLI().Usage = "BACK-37: Migrate all existing data to add required Platform deduplication hash fields"
+	m.CLI().Description = "BACK-37: To fully migrate the Tandem and Omnipod devices from the `jellyfish`\n" +
+		" 	upload API to the `platform` upload API"
+	m.CLI().Authors = []cli.Author{
+		{
+			Name:  "J H BATE",
+			Email: "jamie@tidepool.org",
+		},
+	}
+
+	m.CLI().Action = func(ctx *cli.Context) error {
+		if !m.ParseContext(ctx) {
+			return nil
+		}
+		return m.execute()
+	}
+
+	return nil
+}
+
+func (m *Migration) execute() error {
+	m.Logger().Debug("Migrate jellyfish upload API data")
+	m.Logger().Debug("Creating data store")
+
+	mongoConfig := m.NewMongoConfig()
+	mongoConfig.Database = "data"
+	mongoConfig.Timeout = 60 * time.Minute
+	dataStore, err := storeStructuredMongo.NewStore(mongoConfig)
+	if err != nil {
+		return errors.Wrap(err, "unable to create data store")
+	}
+	defer dataStore.Terminate(context.Background())
+
+	m.Logger().Debug("Creating data repository")
+
+	m.dataRepository = dataStore.GetRepository("deviceData")
+
+	hashUpdatedCount, archivedCount, errorCount := m.migrateJellyfishDocuments()
+
+	m.Logger().Infof("Migrated %d duplicate jellyfish documents", hashUpdatedCount)
+	m.Logger().Infof("Archived %d duplicate jellyfish documents", archivedCount)
+	m.Logger().Infof("%d errors occurred", errorCount)
+
+	return nil
+}
+
+func (m *Migration) migrateJellyfishDocuments() (int, int, int) {
+	logger := m.Logger()
+
+	logger.Debug("Finding distinct users")
+
+	var hashUpdatedCount, archivedCount, errorCount int
+
+	userIDs, err := m.dataRepository.Distinct(context.Background(), "_userId", bson.M{})
+	if err != nil {
+		logger.WithError(err).Error("Unable to execute distinct query")
+	} else {
+		logger.Debugf("Finding jellyfish records for %d users", len(userIDs))
+
+		for _, userID := range userIDs {
+			logger.Debugf("Finding Omnipod records for user ID %s", userID)
+			selector := bson.M{
+				"_userId": userID,
+				"_active": true,
+				// Don't need to change the IDs for uploads, since uploads aren't de-duped.
+				"type":          bson.M{"$ne": "upload"},
+				"_deduplicator": bson.M{"$exists": false},
+			}
+
+			var jellyfishResult bson.M
+			jellyfishDocCursor, err := m.dataRepository.Find(context.Background(), selector)
+			if err != nil {
+				logger.WithError(err).Error("Unable to find jellyfish results")
+			}
+			for jellyfishDocCursor.Next(context.Background()) {
+				jellyfishDocCursor.Decode(&jellyfishResult)
+				hash := JellyfishIDHash(jellyfishResult)
+
+				err = m.migrateDocument(jellyfishResult["_id"], hash)
+				if err != nil {
+					logger.WithError(err).Error("Unable to migrate jellyfish document")
+					errorCount++
+				}
+			}
+			if err := jellyfishDocCursor.Err(); err != nil {
+				logger.WithError(err).Error("error while fetching data. Please re-run to complete the migration.")
+				errorCount++
+			}
+			jellyfishDocCursor.Close(context.Background())
+		}
+	}
+
+	if err != nil {
+		logger.WithError(err).Error("Unable to migrate jellyfish documents")
+		errorCount++
+	}
+
+	return hashUpdatedCount, archivedCount, errorCount
+}
+
+func (m *Migration) migrateDocument(objectId interface{}, hash string) error {
+	deduplicatorUpdate := bson.M{
+		"$set": bson.M{
+			"_deduplicator": bson.M{
+				"name":    deduplicator.DeviceDeactivateHashName,
+				"version": "1.1.0",
+				"hash":    hash,
+			},
+		},
+	}
+	_, err := m.dataRepository.UpdateOne(context.Background(), bson.M{"_id": objectId}, deduplicatorUpdate)
+	return err
+}
