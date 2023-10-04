315a2e8624d62c6308e9f361fc6b1fa6d6b0b54f
Jamie <jamie.h.bate@gmail.com>
2023-10-04 15:25:30 +1300

    cleanup based on disscusion

--- migrations/back_37/back_37.go
+++ migrations/back_37/back_37.go
@@ -31,74 +31,91 @@ type Migration struct {
 	dataRepository *storeStructuredMongo.Repository
 }
 
+func getValidatedString(bsonData bson.M, fieldName string) (string, error) {
+	if valRaw, ok := bsonData[fieldName]; !ok {
+		return "", errors.Newf("%s is missing", fieldName)
+	} else if val, ok := valRaw.(string); !ok {
+		return "", errors.Newf("%s is not of expected type", fieldName)
+	} else if val == "" {
+		return "", errors.Newf("%s is empty", fieldName)
+	} else {
+		return val, nil
+	}
+}
+
+func getValidatedTime(bsonData bson.M, fieldName string) (time.Time, error) {
+	if valRaw, ok := bsonData[fieldName]; !ok {
+		return time.Time{}, errors.Newf("%s is missing", fieldName)
+	} else if val, ok := valRaw.(time.Time); !ok {
+		return time.Time{}, errors.Newf("%s is not of expected type", fieldName)
+	} else if val.IsZero() {
+		return time.Time{}, errors.Newf("%s is empty", fieldName)
+	} else {
+		return val, nil
+	}
+}
+
 func createDatumHash(bsonData bson.M) (string, error) {
 	identityFields := []string{}
-	if bsonData["_userId"] == nil {
-		return "", errors.New("user id is missing")
+	if userID, err := getValidatedString(bsonData, "_userId"); err != nil {
+		return "", err
+	} else {
+		identityFields = append(identityFields, userID)
 	}
-	userID := bsonData["_userId"].(string)
-	if userID == "" {
-		return "", errors.New("user id is empty")
+	if deviceID, err := getValidatedString(bsonData, "deviceId"); err != nil {
+		return "", err
+	} else {
+		identityFields = append(identityFields, deviceID)
 	}
-	identityFields = append(identityFields, userID)
-	if bsonData["deviceId"] == nil {
-		return "", errors.New("device id is missing")
+	if datumTime, err := getValidatedTime(bsonData, "time"); err != nil {
+		return "", err
+	} else {
+		identityFields = append(identityFields, datumTime.Format(types.TimeFormat))
 	}
-	deviceID := bsonData["deviceId"].(string)
-	if deviceID == "" {
-		return "", errors.New("device id is empty")
+	datumType, err := getValidatedString(bsonData, "type")
+	if err != nil {
+		return "", err
 	}
-	identityFields = append(identityFields, deviceID)
-	if bsonData["time"] == nil {
-		return "", errors.New("time is missing")
-	}
-	dataTime := bsonData["time"].(time.Time)
-	if dataTime.IsZero() {
-		return "", errors.New("time is empty")
-	}
-	identityFields = append(identityFields, dataTime.Format(types.TimeFormat))
-	if bsonData["type"] == nil {
-		return "", errors.New("type is missing")
-	}
-	dataType := bsonData["type"].(string)
-	if dataType == "" {
-		return "", errors.New("type is empty")
-	}
-	identityFields = append(identityFields, dataType)
+	identityFields = append(identityFields, datumType)
 
-	switch dataType {
+	switch datumType {
 	case "basal":
-		if bsonData["deliveryType"] == nil {
-			return "", errors.New("deliveryType is missing")
+		if deliveryType, err := getValidatedString(bsonData, "deliveryType"); err != nil {
+			return "", err
+		} else {
+			identityFields = append(identityFields, deliveryType)
 		}
-		deliveryType := bsonData["deliveryType"].(string)
-		if deliveryType == "" {
-			return "", errors.New("deliveryType is empty")
-		}
-		identityFields = append(identityFields, deliveryType)
 	case "bolus", "deviceEvent":
-		if bsonData["subType"] == nil {
-			return "", errors.New("subType is missing")
+		if subType, err := getValidatedString(bsonData, "subType"); err != nil {
+			return "", err
+		} else {
+			identityFields = append(identityFields, subType)
 		}
-		subType := bsonData["subType"].(string)
-		if subType == "" {
-			return "", errors.New("subType is empty")
-		}
-		identityFields = append(identityFields, subType)
 	case "smbg", "bloodKetone", "cbg":
-		if bsonData["units"] == nil {
-			return "", errors.New("units is missing")
+		if units, err := getValidatedString(bsonData, "units"); err != nil {
+			return "", err
+		} else {
+			identityFields = append(identityFields, units)
 		}
-		units := bsonData["units"].(string)
-		if units == "" {
-			return "", errors.New("units is empty")
-		}
-		identityFields = append(identityFields, units)
-		if bsonData["value"] == nil {
+
+		valueRaw, ok := bsonData["value"]
+		if !ok {
 			return "", errors.New("value is missing")
 		}
-		value := strconv.FormatFloat(bsonData["value"].(float64), 'f', -1, 64)
-		identityFields = append(identityFields, value)
+		val, ok := valueRaw.(float64)
+		if !ok {
+			return "", errors.New("value is not of expected type")
+		}
+
+		if len(fmt.Sprintf("%v", valueRaw)) > 7 {
+			// NOTE: we need to ensure the same precision for the
+			// converted value as it is used to calculate the hash
+			mgdlVal := val*18.01559 + 0.5
+			mgdL := glucose.MgdL
+			val = *glucose.NormalizeValueForUnits(&mgdlVal, &mgdL)
+		}
+		strVal := strconv.FormatFloat(val, 'f', -1, 64)
+		identityFields = append(identityFields, strVal)
 	}
 	return deduplicator.GenerateIdentityHash(identityFields)
 }
@@ -148,7 +165,6 @@ func (m *Migration) execute() error {
 	defer dataStore.Terminate(m.ctx)
 
 	m.Logger().Debug("Creating data repository")
-
 	m.dataRepository = dataStore.GetRepository("deviceData")
 	m.Logger().Info("Migration of jellyfish documents has begun")
 	hashUpdatedCount, errorCount := m.migrateJellyfishDocuments()
@@ -198,38 +214,31 @@ func (m *Migration) migrateJellyfishDocuments() (int, int) {
 }
 
 func (m *Migration) migrateDocument(jfDatum bson.M) error {
-	var update bson.M
 
-	switch jfDatum["type"] {
-	case "smbg", "bloodKetone", "cbg":
-		if len(fmt.Sprintf("%v", jfDatum["value"])) > 7 {
-			// NOTE: we need to ensure the same precision for the
-			// converted value as it is used to calculate the hash
-			val := jfDatum["value"].(float64)
-			mgdlVal := val*18.01559 + 0.5
-			mgdL := glucose.MgdL
-			jfDatum["value"] = glucose.NormalizeValueForUnits(&mgdlVal, &mgdL)
-			hash, err := createDatumHash(jfDatum)
-			if err != nil {
-				return err
-			}
-
-			update = bson.M{
-				"$set": bson.M{
-					"_deduplicator": bson.M{"hash": hash},
-					"value":         jfDatum["value"],
-				},
-			}
-		}
-	default:
-		hash, err := createDatumHash(jfDatum)
-		if err != nil {
-			return err
-		}
-		update = bson.M{
-			"$set": bson.M{"_deduplicator": bson.M{"hash": hash}},
-		}
+	datumID, err := getValidatedString(jfDatum, "_id")
+	if err != nil {
+		return err
 	}
-	_, err := m.dataRepository.UpdateOne(m.ctx, bson.M{"_id": jfDatum["_id"], "modifiedTime": jfDatum["modifiedTime"]}, update)
+
+	var modifiedTime *time.Time
+	if timeRaw, ok := jfDatum["modifiedTime"]; !ok {
+		modifiedTime = nil
+	} else if val, ok := timeRaw.(time.Time); !ok {
+		modifiedTime = nil
+	} else {
+		modifiedTime = &val
+	}
+
+	hash, err := createDatumHash(jfDatum)
+	if err != nil {
+		return err
+	}
+	update := bson.M{
+		"$set": bson.M{"_deduplicator": bson.M{"hash": hash}},
+	}
+	_, err = m.dataRepository.UpdateOne(m.ctx, bson.M{
+		"_id":          datumID,
+		"modifiedTime": modifiedTime,
+	}, update)
 	return err
 }
