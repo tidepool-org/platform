82db89b86d74f18087349062027b98258a830d45
lostlevels <lostlevels@users.noreply.github.com>
2024-04-01 07:40:23 -0700

    Have a LegacyUserProfile to support seagull requests.

--- auth/service/api/v1/profile.go
+++ auth/service/api/v1/profile.go
@@ -15,8 +15,10 @@ import (
 func (r *Router) ProfileRoutes() []*rest.Route {
 	return []*rest.Route{
 		rest.Get("/v1/users/:userId/profile", api.RequireUser(r.GetProfile)),
+		rest.Get("/v1/users/:userId/legacy_profile", api.RequireUser(r.GetLegacyProfile)),
 		// The following modification routes required custodian access in seagull, but I'm not sure that's quite right - it seems it should be if the user can modify the userId.
 		rest.Put("/v1/users/:userId/profile", r.requireWriteAccess("userId", r.UpdateProfile)),
+		rest.Put("/v1/users/:userId/legacy_profile", r.requireWriteAccess("userId", r.UpdateLegacyProfile)),
 		rest.Delete("/v1/users/:userId/profile", r.requireWriteAccess("userId", r.DeleteProfile)),
 	}
 }
@@ -41,20 +43,68 @@ func (r *Router) GetProfile(res rest.ResponseWriter, req *rest.Request) {
 		responder.Error(http.StatusBadRequest, err)
 		return
 	}
+	if user == nil || user.Profile == nil {
+		responder.Empty(http.StatusNotFound)
+		return
+	}
 
 	responder.Data(http.StatusOK, user.Profile)
 }
 
-func (r *Router) UpdateProfile(res rest.ResponseWriter, req *rest.Request) {
+func (r *Router) GetLegacyProfile(res rest.ResponseWriter, req *rest.Request) {
 	responder := request.MustNewResponder(res, req)
 	ctx := req.Context()
+	details := request.GetAuthDetails(ctx)
 	userID := req.PathParam("userId")
+	hasPerms, err := r.PermissionsClient().HasMembershipRelationship(ctx, details.UserID(), userID)
+	if err != nil {
+		responder.InternalServerError(err)
+		return
+	}
+	if !hasPerms {
+		responder.Empty(http.StatusForbidden)
+		return
+	}
+
+	user, err := r.UserAccessor().FindUserById(ctx, userID)
+	if err != nil {
+		responder.Error(http.StatusBadRequest, err)
+		return
+	}
+	if user == nil || user.Profile == nil {
+		responder.Empty(http.StatusNotFound)
+		return
+	}
+
+	responder.Data(http.StatusOK, user.Profile.ToLegacyProfile())
+}
+
+func (r *Router) UpdateProfile(res rest.ResponseWriter, req *rest.Request) {
+	responder := request.MustNewResponder(res, req)
 
 	profile := &user.UserProfile{}
 	if err := request.DecodeRequestBody(req.Request, profile); err != nil {
 		responder.Error(http.StatusBadRequest, err)
 		return
 	}
+	r.updateProfile(res, req, profile)
+}
+
+func (r *Router) UpdateLegacyProfile(res rest.ResponseWriter, req *rest.Request) {
+	responder := request.MustNewResponder(res, req)
+
+	profile := &user.LegacyUserProfile{}
+	if err := request.DecodeRequestBody(req.Request, profile); err != nil {
+		responder.Error(http.StatusBadRequest, err)
+		return
+	}
+	r.updateProfile(res, req, profile.ToUserProfile())
+}
+
+func (r *Router) updateProfile(res rest.ResponseWriter, req *rest.Request, profile *user.UserProfile) {
+	responder := request.MustNewResponder(res, req)
+	ctx := req.Context()
+	userID := req.PathParam("userId")
 	if err := structValidator.New().Validate(profile); err != nil {
 		responder.Error(http.StatusBadRequest, err)
 		return
