5396aec63fc8c447f8c4ddb5ecc830ec7e5bccd0
Eric Wollesen <eric@tidepool.org>
2024-05-06 09:53:25 -0600

    adds List and Get methods to alerts client
    The Get endpoint already exists on the service, so only the List endpoint
    needed to be added there.
    
    BACK-2554

--- alerts/client.go
+++ alerts/client.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/tidepool-org/platform/auth"
 	"github.com/tidepool-org/platform/client"
+	"github.com/tidepool-org/platform/errors"
 	platformlog "github.com/tidepool-org/platform/log"
 	"github.com/tidepool-org/platform/log/null"
 	"github.com/tidepool-org/platform/platform"
@@ -42,14 +43,22 @@ type PlatformClient interface {
 		requestBody interface{}, responseBody interface{}, inspectors ...request.ResponseInspector) error
 }
 
+// TokenProvider retrieves session tokens for calling the alerts API.
+//
+// client.External is one implementation
+type TokenProvider interface {
+	// ServerSessionToken provides a server-to-server API authentication token.
+	ServerSessionToken() (string, error)
+}
+
 // request performs common operations before passing a request off to the
 // underlying platform.Client.
-func (c *Client) request(ctx context.Context, method, url string, body any) error {
+func (c *Client) request(ctx context.Context, method, url string, reqBody, resBody any) error {
 	// Platform's client.Client expects a logger to exist in the request's
 	// context. If it doesn't exist, request processing will panic.
 	loggingCtx := platformlog.NewContextWithLogger(ctx, c.logger)
 	// Make sure the auth token is injected into the request's headers.
-	return c.requestWithAuth(loggingCtx, method, url, body)
+	return c.requestWithAuth(loggingCtx, method, url, reqBody, resBody)
 }
 
 // requestWithAuth injects an auth token before calling platform.Client.RequestData.
@@ -58,20 +67,44 @@ func (c *Client) request(ctx context.Context, method, url string, body any) erro
 // platform.Client. It might be nice to be able to use a mutator, but the auth
 // is specifically handled by the platform.Client via the context field, and
 // if left blank, platform.Client errors.
-func (c *Client) requestWithAuth(ctx context.Context, method, url string, body any) error {
-	return c.client.RequestData(auth.NewContextWithServerSessionTokenProvider(ctx, c.tokenProvider), method, url, nil, body, nil)
+func (c *Client) requestWithAuth(ctx context.Context, method, url string, reqBody, resBody any) error {
+	return c.client.RequestData(auth.NewContextWithServerSessionTokenProvider(ctx, c.tokenProvider), method, url, nil, reqBody, resBody)
 }
 
 // Upsert updates cfg if it exists or creates it if it doesn't.
 func (c *Client) Upsert(ctx context.Context, cfg *Config) error {
 	url := c.client.ConstructURL("v1", "users", cfg.FollowedUserID, "followers", cfg.UserID, "alerts")
-	return c.request(ctx, http.MethodPost, url, cfg)
+	return c.request(ctx, http.MethodPost, url, cfg, nil)
 }
 
 // Delete the alerts config.
 func (c *Client) Delete(ctx context.Context, cfg *Config) error {
 	url := c.client.ConstructURL("v1", "users", cfg.FollowedUserID, "followers", cfg.UserID, "alerts")
-	return c.request(ctx, http.MethodDelete, url, nil)
+	return c.request(ctx, http.MethodDelete, url, nil, nil)
+}
+
+// Get a user's alerts configuration for the followed user.
+func (c *Client) Get(ctx context.Context, followedUserID, userID string) (*Config, error) {
+	url := c.client.ConstructURL("v1", "users", followedUserID, "followers", userID, "alerts")
+	cfg := &Config{}
+	err := c.request(ctx, http.MethodGet, url, nil, cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to request alerts config")
+	}
+	return cfg, nil
+}
+
+// List the alerts configurations that follow the given user.
+//
+// This method should only be called via an authenticated service session.
+func (c *Client) List(ctx context.Context, followedUserID string) ([]*Config, error) {
+	url := c.client.ConstructURL("v1", "users", followedUserID, "followers", "alerts")
+	configs := []*Config{}
+	err := c.request(ctx, http.MethodGet, url, nil, &configs)
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to request alerts configs list")
+	}
+	return configs, nil
 }
 
 // ConfigLoader abstracts the method by which config values are loaded.
