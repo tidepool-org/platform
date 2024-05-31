ce673ae9ecad99aa07d4a048daeee96f969ad74d
Eric Wollesen <eric@tidepool.org>
2024-05-31 11:35:26 -0600

    adds a mongodb query for alertable data
    Where alertable data means those data used when evaluating whether a defined
    alert configuration is triggered, and a notification needs to be sent.
    
    BACK-2554

--- data/store/store.go
+++ data/store/store.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/tidepool-org/platform/alerts"
 	"github.com/tidepool-org/platform/data/summary/types"
+	"github.com/tidepool-org/platform/data/types/blood/glucose"
+	"github.com/tidepool-org/platform/data/types/dosingdecision"
 
 	"github.com/tidepool-org/platform/data"
 	"github.com/tidepool-org/platform/data/types/upload"
@@ -67,6 +69,9 @@ type DatumRepository interface {
 	DistinctUserIDs(ctx context.Context, typ string) ([]string, error)
 
 	CheckDataSetContainsTypeInRange(ctx context.Context, dataSetId string, typ string, startTime time.Time, endTime time.Time) (bool, error)
+
+	// GetAlertableData queries for the data used to evaluate alerts configurations.
+	GetAlertableData(ctx context.Context, params AlertableParams) (*AlertableResponse, error)
 }
 
 // DataRepository is the combined interface of DataSetRepository and
@@ -96,4 +101,20 @@ type SummaryRepository interface {
 	EnsureIndexes() error
 
 	GetStore() *storeStructuredMongo.Repository
+}
+
+type AlertableParams struct {
+	// UserID of the user that owns the data.
+	UserID string
+	// UploadID of the device data set to query.
+	UploadID string
+	// Start limits the data to those recorded after this time.
+	Start time.Time
+	// End limits the data to those recorded before this time.
+	End time.Time
+}
+
+type AlertableResponse struct {
+	Glucose         []*glucose.Glucose
+	DosingDecisions []*dosingdecision.DosingDecision
 }
