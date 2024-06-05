97d74dff2884d25e41a7a7e5dd5ec9053f0f20fd
lostlevels <lostlevels@users.noreply.github.com>
2024-06-05 14:10:01 -0700

    Update migration status.

--- user/fallback_user_accessor.go
+++ user/fallback_user_accessor.go
@@ -21,7 +21,7 @@ func (f *FallbackLegacyUserAccessor) FindUserProfile(ctx context.Context, id str
 	if err != nil && !errors.Is(err, ErrUserProfileNotFound) {
 		return nil, err
 	}
-	if seagullProfile != nil && !seagullProfile.Migrated {
+	if seagullProfile != nil && seagullProfile.MigrationStatus == migrationUnmigrated {
 		return seagullProfile, nil
 	}
 	profile, err := f.accessor.FindUserProfile(ctx, id)
@@ -40,7 +40,7 @@ func (f *FallbackLegacyUserAccessor) UpdateUserProfile(ctx context.Context, id s
 		return err
 	}
 	// An unmigrated profile should be returned until the profile has been migrated
-	if seagullProfile != nil && !seagullProfile.Migrated {
+	if seagullProfile != nil && seagullProfile.MigrationStatus == migrationUnmigrated {
 		return f.legacy.UpdateUserProfile(ctx, id, p)
 	}
 	profile := p.ToUserProfile()
@@ -52,7 +52,7 @@ func (f *FallbackLegacyUserAccessor) DeleteUserProfile(ctx context.Context, id s
 	if err != nil && !errors.Is(err, ErrUserProfileNotFound) {
 		return err
 	}
-	if seagullProfile != nil && !seagullProfile.Migrated {
+	if seagullProfile != nil && seagullProfile.MigrationStatus == migrationUnmigrated {
 		return f.legacy.DeleteUserProfile(ctx, id)
 	}
 	return f.accessor.DeleteUserProfile(ctx, id)
