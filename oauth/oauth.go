2bd0136805af27263e76c3170191bcd730bc5c41
Darin Krauss <darin.krauss@tidepool.org>
2024-12-27 19:05:49 -0800

    Use provider session in Provider callbacks
    - Use provider session in Provider callbacks
    - Unravel import cycle between auth and oauth packages

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -4,18 +4,17 @@ import (
 	"context"
 	"net/http"
 	"strings"
-	"time"
 
 	"golang.org/x/oauth2"
 
+	"github.com/tidepool-org/platform/auth"
 	"github.com/tidepool-org/platform/errors"
 	"github.com/tidepool-org/platform/provider"
 	"github.com/tidepool-org/platform/request"
-	"github.com/tidepool-org/platform/structure"
 )
 
 type TokenSourceSource interface {
-	TokenSource(ctx context.Context, token *Token) (oauth2.TokenSource, error)
+	TokenSource(ctx context.Context, token *auth.OAuthToken) (oauth2.TokenSource, error)
 }
 
 type Provider interface {
@@ -24,80 +23,14 @@ type Provider interface {
 
 	CalculateStateForRestrictedToken(restrictedToken string) string // state = crypto of provider name, restrictedToken, secret
 	GetAuthorizationCodeURLWithState(state string) string
-	ExchangeAuthorizationCodeForToken(ctx context.Context, authorizationCode string) (*Token, error)
+	ExchangeAuthorizationCodeForToken(ctx context.Context, authorizationCode string) (*auth.OAuthToken, error)
 }
 
 type TokenSource interface {
 	HTTPClient(ctx context.Context, tokenSourceSource TokenSourceSource) (*http.Client, error)
 
-	RefreshedToken() (*Token, error)
+	RefreshedToken() (*auth.OAuthToken, error)
 	ExpireToken()
-}
-
-type Token struct {
-	AccessToken    string    `json:"accessToken" bson:"accessToken"`
-	TokenType      string    `json:"tokenType,omitempty" bson:"tokenType,omitempty"`
-	RefreshToken   string    `json:"refreshToken,omitempty" bson:"refreshToken,omitempty"`
-	ExpirationTime time.Time `json:"expirationTime,omitempty" bson:"expirationTime,omitempty"`
-}
-
-func NewToken() *Token {
-	return &Token{}
-}
-
-func NewTokenFromRawToken(rawToken *oauth2.Token) (*Token, error) {
-	if rawToken == nil {
-		return nil, errors.New("raw token is missing")
-	}
-
-	return &Token{
-		AccessToken:    rawToken.AccessToken,
-		TokenType:      rawToken.TokenType,
-		RefreshToken:   rawToken.RefreshToken,
-		ExpirationTime: rawToken.Expiry,
-	}, nil
-}
-
-func (t *Token) Parse(parser structure.ObjectParser) {
-	if accessToken := parser.String("accessToken"); accessToken != nil {
-		t.AccessToken = *accessToken
-	}
-	if tokenType := parser.String("tokenType"); tokenType != nil {
-		t.TokenType = *tokenType
-	}
-	if refreshToken := parser.String("refreshToken"); refreshToken != nil {
-		t.RefreshToken = *refreshToken
-	}
-	if expirationTime := parser.Time("expirationTime", time.RFC3339Nano); expirationTime != nil {
-		t.ExpirationTime = *expirationTime
-	}
-}
-
-func (t *Token) Validate(validator structure.Validator) {
-	validator.String("accessToken", &t.AccessToken).NotEmpty()
-}
-
-func (t *Token) Normalize(normalizer structure.Normalizer) {}
-
-func (t *Token) Expire() {
-	t.ExpirationTime = time.Now().Add(-time.Second)
-}
-
-func (t *Token) RawToken() *oauth2.Token {
-	return &oauth2.Token{
-		AccessToken:  t.AccessToken,
-		TokenType:    t.TokenType,
-		RefreshToken: t.RefreshToken,
-		Expiry:       t.ExpirationTime,
-	}
-}
-
-func (t *Token) MatchesRawToken(rawToken *oauth2.Token) bool {
-	return rawToken != nil &&
-		rawToken.AccessToken == t.AccessToken &&
-		rawToken.TokenType == t.TokenType &&
-		rawToken.RefreshToken == t.RefreshToken &&
-		rawToken.Expiry.Equal(t.ExpirationTime)
 }
 
 func IsAccessTokenError(err error) bool {
