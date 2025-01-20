ccd0330c616589eb0ab55ab0efe5d0640145202c
Darin Krauss <darin.krauss@tidepool.org>
2025-01-20 11:38:01 -0800

    Add redwood API routes
    - Add redwood API routes
    - Update go.mod

--- data/service/api/v1/v1.go
+++ data/service/api/v1/v1.go
@@ -1,31 +1,34 @@
 package v1
 
 import (
-	"github.com/tidepool-org/platform/data/service"
-	"github.com/tidepool-org/platform/service/api"
+	redwoodServiceApiV1 "github.com/tidepool-org/platform-plugin-redwood/redwood/service/api/v1"
+
+	dataService "github.com/tidepool-org/platform/data/service"
+	serviceApi "github.com/tidepool-org/platform/service/api"
 )
 
-func Routes() []service.Route {
-	routes := []service.Route{
-		service.Post("/v1/datasets/:dataSetId/data", DataSetsDataCreate, api.RequireAuth),
-		service.Delete("/v1/datasets/:dataSetId", DataSetsDelete, api.RequireAuth),
-		service.Put("/v1/datasets/:dataSetId", DataSetsUpdate, api.RequireAuth),
-		service.Delete("/v1/users/:userId/data", UsersDataDelete, api.RequireAuth),
-		service.Post("/v1/users/:userId/datasets", UsersDataSetsCreate, api.RequireAuth),
-		service.Get("/v1/users/:userId/datasets", UsersDataSetsGet, api.RequireAuth),
+func Routes() []dataService.Route {
+	routes := []dataService.Route{
+		dataService.Post("/v1/datasets/:dataSetId/data", DataSetsDataCreate, serviceApi.RequireAuth),
+		dataService.Delete("/v1/datasets/:dataSetId", DataSetsDelete, serviceApi.RequireAuth),
+		dataService.Put("/v1/datasets/:dataSetId", DataSetsUpdate, serviceApi.RequireAuth),
+		dataService.Delete("/v1/users/:userId/data", UsersDataDelete, serviceApi.RequireAuth),
+		dataService.Post("/v1/users/:userId/datasets", UsersDataSetsCreate, serviceApi.RequireAuth),
+		dataService.Get("/v1/users/:userId/datasets", UsersDataSetsGet, serviceApi.RequireAuth),
 
-		service.Post("/v1/data_sets/:dataSetId/data", DataSetsDataCreate, api.RequireAuth),
-		service.Delete("/v1/data_sets/:dataSetId/data", DataSetsDataDelete, api.RequireAuth),
-		service.Delete("/v1/data_sets/:dataSetId", DataSetsDelete, api.RequireAuth),
-		service.Put("/v1/data_sets/:dataSetId", DataSetsUpdate, api.RequireAuth),
-		service.Get("/v1/time", TimeGet),
-		service.Post("/v1/users/:userId/data_sets", UsersDataSetsCreate, api.RequireAuth),
+		dataService.Post("/v1/data_sets/:dataSetId/data", DataSetsDataCreate, serviceApi.RequireAuth),
+		dataService.Delete("/v1/data_sets/:dataSetId/data", DataSetsDataDelete, serviceApi.RequireAuth),
+		dataService.Delete("/v1/data_sets/:dataSetId", DataSetsDelete, serviceApi.RequireAuth),
+		dataService.Put("/v1/data_sets/:dataSetId", DataSetsUpdate, serviceApi.RequireAuth),
+		dataService.Get("/v1/time", TimeGet),
+		dataService.Post("/v1/users/:userId/data_sets", UsersDataSetsCreate, serviceApi.RequireAuth),
 	}
 
 	routes = append(routes, DataSetsRoutes()...)
 	routes = append(routes, SourcesRoutes()...)
 	routes = append(routes, SummaryRoutes()...)
 	routes = append(routes, AlertsRoutes()...)
+	routes = append(routes, redwoodServiceApiV1.Routes()...)
 
 	return routes
 }
