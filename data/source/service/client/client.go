72847570bfd848d43d7f115431d7436ea3e35dff
Darin Krauss <darin.krauss@tidepool.org>
2025-02-10 21:32:54 -0800

    Remove redundant data source client authorization checks

--- data/source/service/client/client.go
+++ data/source/service/client/client.go
@@ -8,7 +8,6 @@ import (
 	dataSourceStoreStructured "github.com/tidepool-org/platform/data/source/store/structured"
 	"github.com/tidepool-org/platform/errors"
 	"github.com/tidepool-org/platform/page"
-	"github.com/tidepool-org/platform/permission"
 	"github.com/tidepool-org/platform/request"
 )
 
@@ -34,65 +33,26 @@ func New(provider Provider) (*Client, error) {
 // FUTURE: Return ErrorResourceNotFoundWithID(userID) if userID does not exist at all
 
 func (c *Client) List(ctx context.Context, userID string, filter *dataSource.Filter, pagination *page.Pagination) (dataSource.SourceArray, error) {
-	if _, err := c.AuthClient().EnsureAuthorizedUser(ctx, userID, permission.Owner); err != nil {
-		return nil, err
-	}
-
-	repository := c.DataSourceStructuredStore().NewDataSourcesRepository()
-	return repository.List(ctx, userID, filter, pagination)
+	return c.DataSourceStructuredStore().NewDataSourcesRepository().List(ctx, userID, filter, pagination)
 }
 
 func (c *Client) Create(ctx context.Context, userID string, create *dataSource.Create) (*dataSource.Source, error) {
-	if err := c.AuthClient().EnsureAuthorizedService(ctx); err != nil {
-		return nil, err
-	}
-
-	repository := c.DataSourceStructuredStore().NewDataSourcesRepository()
-	return repository.Create(ctx, userID, create)
+	return c.DataSourceStructuredStore().NewDataSourcesRepository().Create(ctx, userID, create)
 }
 
 func (c *Client) DeleteAll(ctx context.Context, userID string) error {
-	if err := c.AuthClient().EnsureAuthorizedService(ctx); err != nil {
-		return err
-	}
-
-	repository := c.DataSourceStructuredStore().NewDataSourcesRepository()
-	_, err := repository.DestroyAll(ctx, userID)
+	_, err := c.DataSourceStructuredStore().NewDataSourcesRepository().DestroyAll(ctx, userID)
 	return err
 }
 
 func (c *Client) Get(ctx context.Context, id string) (*dataSource.Source, error) {
-	if err := c.AuthClient().EnsureAuthorized(ctx); err != nil {
-		return nil, err
-	}
-
-	repository := c.DataSourceStructuredStore().NewDataSourcesRepository()
-	result, err := repository.Get(ctx, id)
-	if err != nil {
-		return nil, err
-	}
-
-	if _, err = c.AuthClient().EnsureAuthorizedUser(ctx, *result.UserID, permission.Owner); err != nil {
-		return nil, err
-	}
-
-	return result, nil
+	return c.DataSourceStructuredStore().NewDataSourcesRepository().Get(ctx, id)
 }
 
 func (c *Client) Update(ctx context.Context, id string, condition *request.Condition, update *dataSource.Update) (*dataSource.Source, error) {
-	if err := c.AuthClient().EnsureAuthorizedService(ctx); err != nil {
-		return nil, err
-	}
-
-	repository := c.DataSourceStructuredStore().NewDataSourcesRepository()
-	return repository.Update(ctx, id, condition, update)
+	return c.DataSourceStructuredStore().NewDataSourcesRepository().Update(ctx, id, condition, update)
 }
 
 func (c *Client) Delete(ctx context.Context, id string, condition *request.Condition) (bool, error) {
-	if err := c.AuthClient().EnsureAuthorizedService(ctx); err != nil {
-		return false, err
-	}
-
-	repository := c.DataSourceStructuredStore().NewDataSourcesRepository()
-	return repository.Destroy(ctx, id, condition)
+	return c.DataSourceStructuredStore().NewDataSourcesRepository().Destroy(ctx, id, condition)
 }
