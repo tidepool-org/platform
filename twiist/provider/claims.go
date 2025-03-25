f4df679e3c2821e1c6a8333fdb93296a2e8d806a
Todd Kazakov <todd.kazakov@gmail.com>
2025-03-25 22:54:53 +0200

    Fix tidepool link id claim name

--- twiist/provider/claims.go
+++ twiist/provider/claims.go
@@ -4,5 +4,5 @@ import "github.com/golang-jwt/jwt/v4"
 
 type Claims struct {
 	jwt.RegisteredClaims
-	TidepoolLinkID string `json:"tidepool_link_id"`
+	TidepoolLinkID string `json:"custom:tidepool_link_id"`
 }
