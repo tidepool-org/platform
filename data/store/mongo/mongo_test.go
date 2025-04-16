cdec4b1bb1fe5a794260030d77ecc5fb36810d91
Alexander Diana <alex@rouk.org>
2025-04-16 22:57:01 +0000

    Modify summary indexes to allow for sorted migratiuon queries

--- data/store/mongo/mongo_test.go
+++ data/store/mongo/mongo_test.go
@@ -420,9 +420,9 @@ var _ = Describe("Mongo", Label("mongodb", "slow", "integration"), func() {
 						"Name":       Equal("UserIDTypeUnique"),
 					}),
 					MatchFields(IgnoreExtras, Fields{
-						"Key":        Equal(storeStructuredMongoTest.MakeKeySlice("type", "dates.outdatedSince", "config.schemaVersion", "dates.lastUpdatedDate")),
+						"Key":        Equal(storeStructuredMongoTest.MakeKeySlice("type", "dates.outdatedSince", "dates.lastUpdatedDate", "config.schemaVersion")),
 						"Background": Equal(false),
-						"Name":       Equal("OutdatedSinceSchemaLastUpdated"),
+						"Name":       Equal("OutdatedAndSchemaMigration"),
 					}),
 				))
 			})
