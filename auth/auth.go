66edac14b00b557ee2eb9e339f3415b7d6c7778d
Darin Krauss <darin.krauss@tidepool.org>
2025-05-17 00:15:55 -0700

    [BACK-3732] Refactor generated gomock mocks using consistent location and naming scheme
    - Refactor generated gomock mocks using consistent location and naming scheme
    - https://tidepool.atlassian.net/browse/BACK-3732

--- auth/auth.go
+++ auth/auth.go
@@ -14,8 +14,7 @@ const (
 	TidepoolRestrictedTokenParameterKey = "restricted_token"
 )
 
-//go:generate mockgen --build_flags=--mod=mod -source=./auth.go -destination=./test/mock.go -package test -aux_files=github.com/tidepool-org/platform/auth=provider_session.go,github.com/tidepool-org/platform/auth=restricted_token.go
-
+//go:generate mockgen -source=auth.go -destination=test/auth_mocks.go -package=test Client
 type Client interface {
 	ProviderSessionAccessor
 	RestrictedTokenAccessor
--- data/source/source.go
+++ data/source/source.go
@@ -35,8 +35,7 @@ func States() []string {
 	}
 }
 
-//go:generate mockgen -destination=./test/mock.go -package test . Client
-
+//go:generate mockgen -source=source.go -destination=test/source_mocks.go -package=test Client
 type Client interface {
 	List(ctx context.Context, userID string, filter *Filter, pagination *page.Pagination) (SourceArray, error)
 	Create(ctx context.Context, userID string, create *Create) (*Source, error)
