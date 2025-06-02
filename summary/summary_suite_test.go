93b829043414fde9533738f7e2457afc0285dd87
Alexander Diana <alex@rouk.org>
2025-06-02 23:40:49 +0000

    formatting

--- summary/summary_suite_test.go
+++ summary/summary_suite_test.go
@@ -6,6 +6,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
 	dataStoreMongo "github.com/tidepool-org/platform/data/store/mongo"
 	storeStructuredMongoTest "github.com/tidepool-org/platform/store/structured/mongo/test"
 )
