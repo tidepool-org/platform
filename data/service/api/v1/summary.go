edc6e495a7aa331dff53058e24c63eac5f7ad644
Todd Kazakov <todd.kazakov@gmail.com>
2026-08-25 11:35:27 +0300

    Remove outdated summary surface and legacy summary date fields

--- data/service/api/v1/summary.go
+++ data/service/api/v1/summary.go
@@ -12,8 +12,8 @@ import (
 
 	"github.com/tidepool-org/platform/clinics"
 	dataService "github.com/tidepool-org/platform/data/service"
+	dataWorkPostprocess "github.com/tidepool-org/platform/data/work/postprocess"
 	"github.com/tidepool-org/platform/errors"
-	"github.com/tidepool-org/platform/page"
 	"github.com/tidepool-org/platform/permission"
 	"github.com/tidepool-org/platform/request"
 	"github.com/tidepool-org/platform/service"
@@ -26,17 +26,9 @@ func SummaryRoutes() []dataService.Route {
 		dataService.Get("/v1/summaries/bgm/:userId", GetSummary[*types.BGMPeriods, *types.GlucoseBucket], api.RequireAuth),
 		dataService.Get("/v1/summaries/con/:userId", GetSummary[*types.ContinuousPeriods, *types.ContinuousBucket], api.RequireAuth),
 
-		dataService.Post("/v1/summaries/cgm/:userId", UpdateSummary[*types.CGMPeriods, *types.GlucoseBucket], api.RequireAuth),
-		dataService.Post("/v1/summaries/bgm/:userId", UpdateSummary[*types.BGMPeriods, *types.GlucoseBucket], api.RequireAuth),
-		dataService.Post("/v1/summaries/con/:userId", UpdateSummary[*types.ContinuousPeriods, *types.ContinuousBucket], api.RequireAuth),
-
-		dataService.Get("/v1/summaries/outdated/cgm", GetOutdatedUserIDs[*types.CGMPeriods, *types.GlucoseBucket], api.RequireAuth),
-		dataService.Get("/v1/summaries/outdated/bgm", GetOutdatedUserIDs[*types.BGMPeriods, *types.GlucoseBucket], api.RequireAuth),
-		dataService.Get("/v1/summaries/outdated/con", GetOutdatedUserIDs[*types.ContinuousPeriods, *types.ContinuousBucket], api.RequireAuth),
-
-		dataService.Get("/v1/summaries/migratable/cgm", GetMigratableUserIDs[*types.CGMPeriods, *types.GlucoseBucket], api.RequireAuth),
-		dataService.Get("/v1/summaries/migratable/bgm", GetMigratableUserIDs[*types.BGMPeriods, *types.GlucoseBucket], api.RequireAuth),
-		dataService.Get("/v1/summaries/migratable/con", GetMigratableUserIDs[*types.ContinuousPeriods, *types.ContinuousBucket], api.RequireAuth),
+		dataService.Post("/v1/summaries/cgm/:userId", UpdateSummary, api.RequireAuth),
+		dataService.Post("/v1/summaries/bgm/:userId", UpdateSummary, api.RequireAuth),
+		dataService.Post("/v1/summaries/con/:userId", UpdateSummary, api.RequireAuth),
 
 		dataService.Get("/v1/clinics/:clinicId/reports/realtime", GetPatientsWithRealtimeData, api.RequireAuth),
 	}
@@ -130,7 +122,9 @@ func GetPatientsWithRealtimeData(dataServiceContext dataService.Context) {
 	responder.Data(http.StatusOK, response)
 }
 
-func UpdateSummary[PP types.PeriodsPt[P, PB, B], PB types.BucketDataPt[B], P types.Periods, B types.BucketData](dataServiceContext dataService.Context) {
+// UpdateSummary reports the data of the user as changed rather than recalculating synchronously,
+// which the retired task runners required. The work created recalculates every summary of the user.
+func UpdateSummary(dataServiceContext dataService.Context) {
 	ctx := dataServiceContext.Request().Context()
 	res := dataServiceContext.Response()
 	req := dataServiceContext.Request()
@@ -143,67 +137,10 @@ func UpdateSummary[PP types.PeriodsPt[P, PB, B], PB types.BucketDataPt[B], P typ
 		return
 	}
 
-	summarizer := summary.GetSummarizer[PP, PB](dataServiceContext.SummarizerRegistry())
-	userSummary, err := summarizer.UpdateSummary(ctx, id)
-	if err != nil {
-		responder.InternalServerError(err)
-	} else {
-		responder.Data(http.StatusOK, userSummary)
-	}
-}
-
-func GetOutdatedUserIDs[PP types.PeriodsPt[P, PB, B], PB types.BucketDataPt[B], P types.Periods, B types.BucketData](dataServiceContext dataService.Context) {
-	ctx := dataServiceContext.Request().Context()
-	res := dataServiceContext.Response()
-	req := dataServiceContext.Request()
-
-	responder := request.MustNewResponder(res, req)
-
-	if details := request.GetAuthDetails(ctx); !details.IsService() {
-		dataServiceContext.RespondWithError(service.ErrorUnauthorized())
-		return
-	}
-
-	pagination := page.NewPagination()
-	if err := request.DecodeRequestQuery(req.Request, pagination); err != nil {
-		responder.Error(http.StatusBadRequest, err)
-		return
-	}
-
-	summarizer := summary.GetSummarizer[PP, PB](dataServiceContext.SummarizerRegistry())
-	response, err := summarizer.GetOutdatedUserIDs(ctx, pagination)
-	if err != nil {
+	if err := dataWorkPostprocess.Enqueue(ctx, dataServiceContext.WorkClient(), id, dataWorkPostprocess.ReasonDataAdded); err != nil {
 		responder.InternalServerError(err)
 		return
 	}
 
-	responder.Data(http.StatusOK, response)
-}
-
-func GetMigratableUserIDs[PP types.PeriodsPt[P, PB, B], PB types.BucketDataPt[B], P types.Periods, B types.BucketData](dataServiceContext dataService.Context) {
-	ctx := dataServiceContext.Request().Context()
-	res := dataServiceContext.Response()
-	req := dataServiceContext.Request()
-
-	responder := request.MustNewResponder(res, req)
-
-	if details := request.GetAuthDetails(ctx); !details.IsService() {
-		dataServiceContext.RespondWithError(service.ErrorUnauthorized())
-		return
-	}
-
-	pagination := page.NewPagination()
-	if err := request.DecodeRequestQuery(req.Request, pagination); err != nil {
-		responder.Error(http.StatusBadRequest, err)
-		return
-	}
-
-	summarizer := summary.GetSummarizer[PP, PB](dataServiceContext.SummarizerRegistry())
-	userIDs, err := summarizer.GetMigratableUserIDs(ctx, pagination)
-	if err != nil {
-		responder.InternalServerError(err)
-		return
-	}
-
-	responder.Data(http.StatusOK, userIDs)
+	responder.Empty(http.StatusAccepted)
 }
